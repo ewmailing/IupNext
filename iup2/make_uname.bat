3043f6c5bb619e52a025aa5c2e90e70e7a323bec
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2008-10-29 18:57:54 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@17 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup2/make_uname.bat
+++ iup2/make_uname.bat
@@ -1,4 +1,5 @@
 @echo off
 REM This builds all the libraries for 1 uname
                                   
-FOR %%u IN (src srccd srccontrols srcpplot srcgl srcim srcole srclua3 srclua5 srcconsole srcledc srcview) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9                                  
+REM FOR %%u IN (src srccd srccontrols srcpplot srcgl srcim srcole srclua3 srclua5 srcconsole srcledc srcview) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9                                  
+FOR %%u IN (src srccd srccontrols srcpplot srcgl srcim srcole srclua5 srcconsole srcledc srcview) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9                                  
