ec5a0dcca725d16a4cc803726b9c032474c22aaf
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2008-11-26 17:49:38 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@97 2f08bc20-984a-4df8-b195-9b5d5d477537

---
+++ iup/srclua5/text.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+ * Automatically generated file (iuplua5). Please don't change anything.                *
+ *****************************************************************************/
+
+#include <stdlib.h>
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#include "iup.h"
+#include "iuplua.h"
+#include "il.h"
+
+
+static int text_action(Ihandle *self, int p0, char * p1)
+{
+  lua_State *L = iuplua_call_start(self, "action");
+  lua_pushnumber(L, p0);
+  lua_pushstring(L, p1);
+  return iuplua_call(L, 2);
+}
+
+static int text_caret_cb(Ihandle *self, int p0, int p1)
+{
+  lua_State *L = iuplua_call_start(self, "caret_cb");
+  lua_pushnumber(L, p0);
+  lua_pushnumber(L, p1);
+  return iuplua_call(L, 2);
+}
+
+static int Text(lua_State *L)
+{
+  Ihandle *ih = IupText(NULL);
+  iuplua_plugstate(L, ih);
+  iuplua_pushihandle_raw(L, ih);
+  return 1;
+}
+
+int iuptextlua_open(lua_State * L)
+{
+  iuplua_register(L, Text, "Text");
+
+  iuplua_register_cb(L, "ACTION", (lua_CFunction)text_action, "text");
+  iuplua_register_cb(L, "CARET_CB", (lua_CFunction)text_caret_cb, NULL);
+
+#ifdef IUPLUA_USELOH
+#ifdef TEC_BIGENDIAN
+#ifdef TEC_64
+#include "loh/text_be64.loh"
+#else
+#include "loh/text_be32.loh"
+#endif
+#else
+#ifdef TEC_64
+#ifdef WIN64
+#include "loh/text_le64w.loh"
+#else
+#include "loh/text_le64.loh"
+#endif
+#else
+#include "loh/text.loh"
+#endif
+#endif
+#else
+  iuplua_dofile(L, "text.lua");
+#endif
+
+  return 0;
+}
+
