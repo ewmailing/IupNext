44f706cf661971ff8131243660511a157471efed
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2008-12-15 05:50:57 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@154 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcconsole/make_uname.bat
+++ iup/srcconsole/make_uname.bat
@@ -26,22 +26,22 @@ goto end
 
 :iupexe32
 call tecmake dll8 relink %2 %3 %4 %5 %6 %7
-call tecmake dll8 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll8 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
 REM call tecmake vc8 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
 
 :iupexe64
 call tecmake dll8_64 relink %2 %3 %4 %5 %6 %7
-call tecmake dll8_64 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll8_64 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
 REM call tecmake vc8_64 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
 
 :iupexe
 call tecmake dll8 relink %2 %3 %4 %5 %6 %7
-call tecmake dll8 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll8 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
 REM call tecmake vc8 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 call tecmake dll8_64 relink %2 %3 %4 %5 %6 %7
-call tecmake dll8_64 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll8_64 USE_GTK=Yes relink %2 %3 %4 %5 %6 %7
 REM call tecmake vc8_64 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
 
