7be492512e07091f22db5c3dccde43d56fd4dcb7
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2009-10-07 17:42:52 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@461 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/C/lua_init.c
+++ iup/html/examples/C/lua_init.c
@@ -1,3 +1,6 @@
+/* Alternative initialization when statically linking IUP and Lua.
+   Usually Iup initialization functions are not necessary because 
+   they are called from inside the IupLua initialization functions.  */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -25,7 +28,7 @@ int main(int argc, char **argv)
   luaL_openlibs(L);
 
   iuplua_open(L);      /* Initialize Binding Lua */
-//  iupcontrolslua_open(L); /* Initialize CPI controls binding Lua */
+//  iupcontrolslua_open(L); /* Initialize the additional controls binding Lua */
 
   /* do other things, like running a lua script */
   luaL_dofile(L, "lua_init.lua");
