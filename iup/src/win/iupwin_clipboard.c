62c2f310e19308942aa878739dc9d75959997eaf
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2010-06-12 22:37:51 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@751 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/win/iupwin_clipboard.c
+++ iup/src/win/iupwin_clipboard.c
@@ -20,6 +20,136 @@
 #include "iupwin_drv.h"
 
 
+typedef struct _APMFILEHEADER
+{
+  WORD  key1,
+        key2,
+        hmf,
+        bleft, btop, bright, bbottom,
+        inch,
+        reserved1,
+        reserved2,
+        checksum;
+} APMFILEHEADER;
+
+static WORD winAPMChecksum(APMFILEHEADER* papm)
+{
+  WORD* pw = (WORD*)papm;
+  WORD  wSum = 0;
+  int   i;
+  
+  /* The checksum in a Placeable Metafile header is calculated */
+  /* by XOR-ing the first 10 words of the header.              */
+  
+  for (i = 0; i < 10; i++)
+    wSum ^= *pw++;
+  
+  return wSum;
+}
+
+static void winWritePlacebleFile(HANDLE hFile, unsigned char* buffer, DWORD dwSize, LONG mm, LONG xExt, LONG yExt)
+{
+  DWORD nBytesWrite;
+  APMFILEHEADER APMHeader;
+  int w = xExt, h = yExt;
+  
+  if (mm == MM_ANISOTROPIC || mm == MM_ISOTROPIC)
+  {
+    int res = 30;
+    w = xExt / res;
+    h = yExt / res;
+  }
+  
+  APMHeader.key1 = 0xCDD7;
+  APMHeader.key2 = 0x9AC6;
+  APMHeader.hmf = 0;
+  APMHeader.bleft = 0;
+  APMHeader.btop = 0;
+  APMHeader.bright = (short)w;
+  APMHeader.bbottom = (short)h;
+  APMHeader.inch = 100;  /* this number works fine in Word, etc.. */
+  APMHeader.reserved1 = 0;
+  APMHeader.reserved2 = 0;
+  APMHeader.checksum = winAPMChecksum(&APMHeader);
+  
+  WriteFile(hFile, (LPSTR)&APMHeader, sizeof(APMFILEHEADER), &nBytesWrite, NULL);
+  WriteFile(hFile, buffer, dwSize, &nBytesWrite, NULL);
+}
+
+static int winClipboardSetSaveEMFAttrib(Ihandle *ih, const char *value)
+{
+  HENHMETAFILE Handle;
+  DWORD dwSize, nBytesWrite;
+  unsigned char* buffer;
+  HANDLE hFile;
+  (void)ih;
+  
+  OpenClipboard(NULL);
+  Handle = (HENHMETAFILE)GetClipboardData(CF_ENHMETAFILE);
+  if (Handle == NULL)
+  {
+    CloseClipboard();
+    return 0;
+  }
+  
+  dwSize = GetEnhMetaFileBits(Handle, 0, NULL);
+  
+  buffer = (unsigned char*)malloc(dwSize);
+  
+  GetEnhMetaFileBits(Handle, dwSize, buffer);
+  
+  hFile = CreateFile(value, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+  if (hFile)
+  {
+    WriteFile(hFile, buffer, dwSize, &nBytesWrite, NULL);
+    CloseHandle(hFile);
+  }
+  
+  free(buffer);
+  
+  CloseClipboard();
+  return 0;
+}
+
+static int winClipboardSetSaveWMFAttrib(Ihandle *ih, const char *value)
+{
+  DWORD dwSize;
+  unsigned char* buffer;
+  METAFILEPICT* lpMFP;
+  HANDLE Handle;
+  HANDLE hFile;
+  (void)ih;
+  
+  OpenClipboard(NULL);
+  Handle = (HENHMETAFILE)GetClipboardData(CF_METAFILEPICT);
+  if (Handle == NULL)
+  {
+    CloseClipboard();
+    return 0;
+  }
+  
+  lpMFP = (METAFILEPICT*) GlobalLock(Handle);
+  
+  dwSize = GetMetaFileBitsEx(lpMFP->hMF, 0, NULL);
+  
+  buffer = (unsigned char*)malloc(dwSize);
+  
+  GetMetaFileBitsEx(lpMFP->hMF, dwSize, buffer);
+  
+  hFile = CreateFile(value, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+  if (hFile)
+  {
+    winWritePlacebleFile(hFile, buffer, dwSize, lpMFP->mm, lpMFP->xExt, lpMFP->yExt);
+    CloseHandle(hFile);
+  }
+  
+  GlobalUnlock(Handle);
+  free(buffer);
+  
+  CloseClipboard();
+  return 0;
+}
+
 static int winClipboardSetTextAttrib(Ihandle *ih, const char *value)
 {
   HANDLE hHandle;
@@ -162,6 +292,32 @@ static char* winClipboardGetImageAvailableAttrib(Ihandle *ih)
     return "NO";
 }
 
+static char* winClipboardGetWMFAvailableAttrib(Ihandle *ih)
+{
+  int check;
+  (void)ih;
+  OpenClipboard(NULL);
+  check = IsClipboardFormatAvailable(CF_METAFILEPICT);
+  CloseClipboard();
+  if (check)
+    return "YES";
+  else
+    return "NO";
+}
+
+static char* winClipboardGetEMFAvailableAttrib(Ihandle *ih)
+{
+  int check;
+  (void)ih;
+  OpenClipboard(NULL);
+  check = IsClipboardFormatAvailable(CF_ENHMETAFILE);
+  CloseClipboard();
+  if (check)
+    return "YES";
+  else
+    return "NO";
+}
+
 /******************************************************************************/
 
 Ihandle* IupClipboard(void)
@@ -185,6 +341,10 @@ Iclass* iupClipboardGetClass(void)
   iupClassRegisterAttribute(ic, "IMAGE", NULL, winClipboardSetImageAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "TEXTAVAILABLE", winClipboardGetTextAvailableAttrib, NULL, NULL, NULL, IUPAF_READONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "IMAGEAVAILABLE", winClipboardGetImageAvailableAttrib, NULL, NULL, NULL, IUPAF_READONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "WMFAVAILABLE", winClipboardGetWMFAvailableAttrib, NULL, NULL, NULL, IUPAF_READONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "EMFAVAILABLE", winClipboardGetEMFAvailableAttrib, NULL, NULL, NULL, IUPAF_READONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "SAVEEMF", NULL, winClipboardSetSaveEMFAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "SAVEWMF", NULL, winClipboardSetSaveWMFAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NOT_MAPPED|IUPAF_NO_INHERIT);
 
   return ic;
 }
