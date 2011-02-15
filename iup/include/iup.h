790e766166fa25790f224aa326755d384e236362
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2011-02-15 18:42:21 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1055 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/include/iup.h
+++ iup/include/iup.h
@@ -23,7 +23,7 @@ extern "C" {
 #define IUP_DESCRIPTION	"Portable toolkit for building graphical user interfaces."
 #define IUP_VERSION "3.4"         /* bug fixes are reported only by IupVersion functions */
 #define IUP_VERSION_NUMBER 304000
-#define IUP_VERSION_DATE "2011/02/XX"  /* does not include bug fix releases */
+#define IUP_VERSION_DATE "2011/02/15"  /* does not include bug fix releases */
 
 typedef struct Ihandle_ Ihandle;
 typedef int (*Icallback)(Ihandle*);
