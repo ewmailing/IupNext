05c9491f26945184f5fa9d89054d52b5e7b6a9e9
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2011-03-01 20:24:38 +0000

    Fixed: ACTION_CB callback not being called in IupMatrix when editing the cell and a non character key was pressed.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1072 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srccontrols/matrix/iupmat_edit.c
+++ iup/srccontrols/matrix/iupmat_edit.c
@@ -296,7 +296,7 @@ static int iMatrixEditTextKeyAny_CB(Ihandle* ih, int c)
 {
   Ihandle* ih_matrix = ih->parent;
   IFniiiis cb = (IFniiiis) IupGetCallback(ih_matrix, "ACTION_CB");
-  if (cb && c==0) /* only for other characters */
+  if (cb && !iup_isprint(c)) /* only for other keys that are not characters */
   {
     int oldc = c;
     c = cb(ih_matrix, c, ih_matrix->data->lines.focus_cell, ih_matrix->data->columns.focus_cell, 1, IupGetAttribute(ih, "VALUE"));
