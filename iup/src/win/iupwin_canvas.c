89040d23199c854af241f37c86e6995c5ee8ba9c
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2011-06-09 17:34:07 +0000

    added missing HDC_WMPAINT attribute registration
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1157 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/win/iupwin_canvas.c
+++ iup/src/win/iupwin_canvas.c
@@ -871,6 +871,7 @@ void iupdrvCanvasInitClass(Iclass* ic)
   /* IupCanvas Windows only */
   iupClassRegisterAttribute(ic, "DRAGDROP", NULL, iupwinSetDragDropAttrib, NULL, NULL, IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "HWND", iupBaseGetWidAttrib, NULL, NULL, NULL, IUPAF_NO_STRING|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HDC_WMPAINT", NULL, NULL, NULL, NULL, IUPAF_NO_STRING|IUPAF_NO_INHERIT);
 
   /* Not Supported */
   iupClassRegisterAttribute(ic, "BACKINGSTORE", NULL, NULL, "YES", NULL, IUPAF_NOT_SUPPORTED|IUPAF_NO_INHERIT);
