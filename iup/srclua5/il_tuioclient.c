63b5bff7e57c4bcfc84679fce9b560bbcfeb863a
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2012-03-29 17:31:56 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1338 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/il_tuioclient.c
+++ iup/srclua5/il_tuioclient.c
@@ -13,16 +13,6 @@
 #include "il.h"
 
 
-static int tuioclient_touch_cb(Ihandle *self, int p0, int p1, int p2, char * p3)
-{
-  lua_State *L = iuplua_call_start(self, "touch_cb");
-  lua_pushinteger(L, p0);
-  lua_pushinteger(L, p1);
-  lua_pushinteger(L, p2);
-  lua_pushstring(L, p3);
-  return iuplua_call(L, 4);
-}
-
 static int TuioClient(lua_State *L)
 {
   Ihandle *ih = IupTuioClient(luaL_optinteger(L, 1, 0));
@@ -35,7 +25,6 @@ int iuptuioclientlua_open(lua_State * L)
 {
   iuplua_register(L, TuioClient, "TuioClient");
 
-  iuplua_register_cb(L, "TOUCH_CB", (lua_CFunction)tuioclient_touch_cb, NULL);
 
 #ifdef IUPLUA_USELOH
 #include "tuioclient.loh"
@@ -51,47 +40,6 @@ int iuptuioclientlua_open(lua_State * L)
 }
 
  
-static int tuio_multitouch_cb(Ihandle *ih, int count, int* id, int* px, int* py, int* pstate)
-{
-  int i;
-  lua_State *L = iuplua_call_start(ih, "multitouch_cb");
-  lua_pushinteger(L, count);
-  
-  lua_createtable(L, count, 0);
-  for (i = 0; i < count; i++)
-  {
-    lua_pushinteger(L,i+1);
-    lua_pushinteger(L,id[i]);
-    lua_settable(L,-3);
-  }
-  
-  lua_createtable(L, count, 0);
-  for (i = 0; i < count; i++)
-  {
-    lua_pushinteger(L,i+1);
-    lua_pushinteger(L,px[i]);
-    lua_settable(L,-3);
-  }
-  
-  lua_createtable(L, count, 0);
-  for (i = 0; i < count; i++)
-  {
-    lua_pushinteger(L,i+1);
-    lua_pushinteger(L,py[i]);
-    lua_settable(L,-3);
-  }
-  
-  
-  lua_createtable(L, count, 0);
-  for (i = 0; i < count; i++)
-  {
-    lua_pushinteger(L,i+1);
-    lua_pushinteger(L,pstate[i]);
-    lua_settable(L,-3);
-  }
-  
-  return iuplua_call(L, 5);
-}
 
 int iuptuiolua_open(lua_State* L)
 {
@@ -100,8 +48,6 @@ int iuptuiolua_open(lua_State* L)
     
   iuplua_get_env(L);
   iuptuioclientlua_open(L);
-  
-  iuplua_register_cb(L, "MULTITOUCH_CB", (lua_CFunction)tuio_multitouch_cb, NULL);
   
   return 0;
 }
