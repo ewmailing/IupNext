65857e001dbe2aeea8f49cf32d86ebc7f9541c75
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2012-05-17 14:51:04 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1386 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/il_matrix.c
+++ iup/srclua5/il_matrix.c
@@ -106,6 +106,15 @@ static int matrix_dropcheck_cb(Ihandle *self, int p0, int p1)
   return iuplua_call(L, 2);
 }
 
+static int matrix_menudrop_cb(Ihandle *self, Ihandle * p0, int p1, int p2)
+{
+  lua_State *L = iuplua_call_start(self, "menudrop_cb");
+  iuplua_pushihandle(L, p0);
+  lua_pushinteger(L, p1);
+  lua_pushinteger(L, p2);
+  return iuplua_call(L, 3);
+}
+
 static int matrix_action_cb(Ihandle *self, int p0, int p1, int p2, int p3, char * p4)
 {
   lua_State *L = iuplua_call_start(self, "action_cb");
@@ -149,6 +158,14 @@ static int matrix_fgcolor_cb(Ihandle *self, int p0, int p1)
   return iuplua_call(L, 2);
 }
 
+static int matrix_mousemove_cb(Ihandle *self, int p0, int p1)
+{
+  lua_State *L = iuplua_call_start(self, "mousemove_cb");
+  lua_pushinteger(L, p0);
+  lua_pushinteger(L, p1);
+  return iuplua_call(L, 2);
+}
+
 static int matrix_release_cb(Ihandle *self, int p0, int p1, char * p2)
 {
   lua_State *L = iuplua_call_start(self, "release_cb");
@@ -156,14 +173,6 @@ static int matrix_release_cb(Ihandle *self, int p0, int p1, char * p2)
   lua_pushinteger(L, p1);
   lua_pushstring(L, p2);
   return iuplua_call(L, 3);
-}
-
-static int matrix_mousemove_cb(Ihandle *self, int p0, int p1)
-{
-  lua_State *L = iuplua_call_start(self, "mousemove_cb");
-  lua_pushinteger(L, p0);
-  lua_pushinteger(L, p1);
-  return iuplua_call(L, 2);
 }
 
 static int matrix_value_edit_cb(Ihandle *self, int p0, int p1, char * p2)
@@ -208,13 +217,14 @@ int iupmatrixlua_open(lua_State * L)
   iuplua_register_cb(L, "DROPSELECT_CB", (lua_CFunction)matrix_dropselect_cb, NULL);
   iuplua_register_cb(L, "DROP_CB", (lua_CFunction)matrix_drop_cb, NULL);
   iuplua_register_cb(L, "DROPCHECK_CB", (lua_CFunction)matrix_dropcheck_cb, NULL);
+  iuplua_register_cb(L, "MENUDROP_CB", (lua_CFunction)matrix_menudrop_cb, NULL);
   iuplua_register_cb(L, "ACTION_CB", (lua_CFunction)matrix_action_cb, "matrix");
   iuplua_register_cb(L, "ENTERITEM_CB", (lua_CFunction)matrix_enteritem_cb, NULL);
   iuplua_register_cb(L, "LEAVEITEM_CB", (lua_CFunction)matrix_leaveitem_cb, NULL);
   iuplua_register_cb(L, "SCROLLTOP_CB", (lua_CFunction)matrix_scrolltop_cb, NULL);
   iuplua_register_cb(L, "FGCOLOR_CB", (lua_CFunction)matrix_fgcolor_cb, NULL);
-  iuplua_register_cb(L, "RELEASE_CB", (lua_CFunction)matrix_release_cb, NULL);
   iuplua_register_cb(L, "MOUSEMOVE_CB", (lua_CFunction)matrix_mousemove_cb, "matrix");
+  iuplua_register_cb(L, "RELEASE_CB", (lua_CFunction)matrix_release_cb, NULL);
   iuplua_register_cb(L, "VALUE_EDIT_CB", (lua_CFunction)matrix_value_edit_cb, NULL);
   iuplua_register_cb(L, "CLICK_CB", (lua_CFunction)matrix_click_cb, NULL);
 
