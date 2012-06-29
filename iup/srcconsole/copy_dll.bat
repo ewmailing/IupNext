804855a7be19ede45ccd8f677c02530514ac486c
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2012-06-29 17:48:22 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1443 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcconsole/copy_dll.bat
+++ iup/srcconsole/copy_dll.bat
@@ -4,7 +4,7 @@ copy /y ..\lib\dll8\*.dll ..\bin\Win32\Lua5.1
 copy /y ..\..\cd\lib\dll8\*.dll ..\bin\Win32\Lua5.1
 copy /y ..\..\im\lib\dll8\*.dll ..\bin\Win32\Lua5.1
 copy /y ..\..\lua5.1\lib\dll8\*.dll ..\bin\Win32\Lua5.1
-copy /y ..\..\luagl\lib5.1\dll8\*.dll ..\bin\Win32\Lua5.1
+copy /y ..\..\luagl\lib\dll8\*.dll ..\bin\Win32\Lua5.1
 copy /y ..\..\lfs\lib5.1\dll8\*.dll ..\bin\Win32\Lua5.1
 mkdir ..\bin\Win32\Lua5.1\Microsoft.VC80.CRT
 copy /y ..\..\lua5.1\bin\Win32\Microsoft.VC80.CRT ..\bin\Win32\Lua5.1\Microsoft.VC80.CRT\
@@ -17,7 +17,7 @@ copy /y ..\lib\dll10\*.dll ..\bin\Win32\Lua52
 copy /y ..\..\cd\lib\dll10\*.dll ..\bin\Win32\Lua52
 copy /y ..\..\im\lib\dll10\*.dll ..\bin\Win32\Lua52
 copy /y ..\..\lua52\lib\dll10\*.dll ..\bin\Win32\Lua52
-copy /y ..\..\luagl\lib52\dll10\*.dll ..\bin\Win32\Lua52
+copy /y ..\..\luagl\lib\dll10\*.dll ..\bin\Win32\Lua52
 copy /y ..\..\lfs\lib52\dll10\*.dll ..\bin\Win32\Lua52
 copy /y ..\dist\Microsoft.VC100.CRT\dll10\* ..\bin\Win32\Lua52
 del ..\bin\Win32\Lua52\*3.dll
--- iup/srcconsole/copy_dll_64.bat
+++ iup/srcconsole/copy_dll_64.bat
@@ -4,7 +4,7 @@ copy /y ..\lib\dll8_64\*.dll ..\bin\Win64\Lua5.1
 copy /y ..\..\cd\lib\dll8_64\*.dll ..\bin\Win64\Lua5.1
 copy /y ..\..\im\lib\dll8_64\*.dll ..\bin\Win64\Lua5.1
 copy /y ..\..\lua5.1\lib\dll8_64\*.dll ..\bin\Win64\Lua5.1
-copy /y ..\..\luagl\lib5.1\dll8_64\*.dll ..\bin\Win64\Lua5.1
+copy /y ..\..\luagl\lib\dll8_64\*.dll ..\bin\Win64\Lua5.1
 copy /y ..\..\lfs\lib5.1\dll8_64\*.dll ..\bin\Win64\Lua5.1
 mkdir ..\bin\Win64\Lua5.1\Microsoft.VC80.CRT
 copy /y ..\..\lua5.1\bin\Win64\Microsoft.VC80.CRT ..\bin\Win64\Lua5.1\Microsoft.VC80.CRT\
@@ -17,7 +17,7 @@ copy /y ..\lib\dll10_64\*.dll ..\bin\Win64\Lua52
 copy /y ..\..\cd\lib\dll10_64\*.dll ..\bin\Win64\Lua52
 copy /y ..\..\im\lib\dll10_64\*.dll ..\bin\Win64\Lua52
 copy /y ..\..\lua52\lib\dll10_64\*.dll ..\bin\Win64\Lua52
-copy /y ..\..\luagl\lib52\dll10_64\*.dll ..\bin\Win64\Lua52
+copy /y ..\..\luagl\lib\dll10_64\*.dll ..\bin\Win64\Lua52
 copy /y ..\..\lfs\lib52\dll10_64\*.dll ..\bin\Win64\Lua52
 copy /y ..\dist\Microsoft.VC100.CRT\dll10_64\* ..\bin\Win64\Lua52
 del ..\bin\Win64\Lua52\*3.dll
