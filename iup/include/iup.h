45c8cc33c4b9225966282e716cd8e868a1f5f70c
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2013-05-07 20:34:42 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1646 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/include/iup.h
+++ iup/include/iup.h
@@ -19,11 +19,11 @@ extern "C" {
 
 
 #define IUP_NAME "IUP - Portable User Interface"
-#define IUP_COPYRIGHT  "Copyright (C) 1994-2012 Tecgraf, PUC-Rio."
+#define IUP_COPYRIGHT  "Copyright (C) 1994-2013 Tecgraf, PUC-Rio."
 #define IUP_DESCRIPTION	"Multi-platform toolkit for building graphical user interfaces."
-#define IUP_VERSION "3.7"         /* bug fixes are reported only by IupVersion functions */
-#define IUP_VERSION_NUMBER 307000
-#define IUP_VERSION_DATE "2012/11/29"  /* does not include bug fix releases */
+#define IUP_VERSION "3.8"         /* bug fixes are reported only by IupVersion functions */
+#define IUP_VERSION_NUMBER 308000
+#define IUP_VERSION_DATE "2013/05/08"  /* does not include bug fix releases */
 
 typedef struct Ihandle_ Ihandle;
 typedef int (*Icallback)(Ihandle*);
@@ -389,7 +389,7 @@ int IupMain (int argc, char** argv); /* In C++ we have to declare the prototype 
 #endif
 
 /******************************************************************************
-* Copyright (C) 1994-2012 Tecgraf, PUC-Rio.
+* Copyright (C) 1994-2013 Tecgraf, PUC-Rio.
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
