4b64be9d99dca1c36a62a1cb717afcc68d2e8610
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2013-06-25 22:16:46 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1695 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcmatrixex/iupmatex_clipboard.c
+++ iup/srcmatrixex/iupmatex_clipboard.c
@@ -174,100 +174,144 @@ static int iMatrixExCellMarkedConsistent(const char* marked, int num_lin, int nu
   return 1;
 }
 
-static int iMatrixExSetCopyAttrib(Ihandle *ih, const char* value)
+static void iMatrixExSetCopyData(Ihandle *ih, Iarray* data, const char* value)
 {
   int lin, col, num_lin, num_col;
   sIFnii value_cb;
-  Iarray* data;
   int add_sep;
-  char *marked;
-
-  /* unused */
-  (void)value;
 
   /* reset error state */
   iupAttribSetStr(ih, "LASTERROR", NULL);
 
-  marked = IupGetAttribute(ih,"MARKED");
-  if (!marked)  /* no marked cells */
-  {
-    iupAttribSetStr(ih, "LASTERROR", "NOMARKED");
-    return 0;
-  }
-
   num_lin = IupGetInt(ih, "NUMLIN");
   num_col = IupGetInt(ih, "NUMCOL");
   value_cb = (sIFnii)IupGetCallback(ih, "VALUE_CB");
-  data =  iupArrayCreate(100, sizeof(char));
 
-  if (*marked == 'C')
+  if (!value || iupStrEqualNoCase(value, "MARKED"))
   {
-    marked++;
-
-    for(lin = 1; lin <= num_lin; ++lin)
+    char *marked = IupGetAttribute(ih,"MARKED");
+    if (!marked)  /* no marked cells */
     {
-      add_sep = 0;
-
-      if (iupMatrixExIsLineVisible(ih, lin))
-      {
-        for(col = 1; col <= num_col; ++col)
-        {
-          if (marked[col-1] == '1' && iupMatrixExIsColumnVisible(ih, col))
-          {
-            if (add_sep)
-              iMatrixExArrayAddChar(data, '\t');
-
-            iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
-            add_sep = 1;
-          }
-        }
-      }
-
-      if (add_sep)
-        iMatrixExArrayAddChar(data, '\n');
-    }
-  }
-  else if (*marked == 'L')
-  {
-    marked++;
-
-    for(lin = 1; lin <= num_lin; ++lin)
-    {
-      add_sep = 0;
-
-      if (iupMatrixExIsLineVisible(ih, lin))
-      {
-        for(col = 1; col <= num_col; ++col)
-        {
-          if (marked[lin-1] == '1' && iupMatrixExIsColumnVisible(ih, col))
-          {
-            if (add_sep)
-              iMatrixExArrayAddChar(data, '\t');
-
-            iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
-            add_sep = 1;
-          }
-        }
-      }
-
-      if (add_sep)
-        iMatrixExArrayAddChar(data, '\n');
-    }
-  }
-  else
-  {
-    int lin1=0, lin2=num_lin, 
-        col1=0, col2=num_col;
-    int keep_struct = IupGetInt(ih, "COPYKEEPSTRUCT");
-
-    iMatrixExCellMarkedLimits(marked, num_lin, num_col, &lin1, &lin2, &col1, &col2);
-
-    /* check consistency only when not keeping structure */
-    if (!keep_struct && !iMatrixExCellMarkedConsistent(marked, num_lin, num_col))
-    {
-      iupAttribSetStr(ih, "LASTERROR", "MARKEDCONSISTENCY");
+      iupAttribSetStr(ih, "LASTERROR", "NOMARKED");
       iupArrayDestroy(data);
-      return 0;
+      return;
+    }
+
+    if (*marked == 'C')
+    {
+      marked++;
+
+      for(lin = 1; lin <= num_lin; ++lin)
+      {
+        add_sep = 0;
+
+        if (iupMatrixExIsLineVisible(ih, lin))
+        {
+          for(col = 1; col <= num_col; ++col)
+          {
+            if (marked[col-1] == '1' && iupMatrixExIsColumnVisible(ih, col))
+            {
+              if (add_sep)
+                iMatrixExArrayAddChar(data, '\t');
+
+              iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
+              add_sep = 1;
+            }
+          }
+        }
+
+        if (add_sep)
+          iMatrixExArrayAddChar(data, '\n');
+      }
+    }
+    else if (*marked == 'L')
+    {
+      marked++;
+
+      for(lin = 1; lin <= num_lin; ++lin)
+      {
+        add_sep = 0;
+
+        if (iupMatrixExIsLineVisible(ih, lin))
+        {
+          for(col = 1; col <= num_col; ++col)
+          {
+            if (marked[lin-1] == '1' && iupMatrixExIsColumnVisible(ih, col))
+            {
+              if (add_sep)
+                iMatrixExArrayAddChar(data, '\t');
+
+              iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
+              add_sep = 1;
+            }
+          }
+        }
+
+        if (add_sep)
+          iMatrixExArrayAddChar(data, '\n');
+      }
+    }
+    else
+    {
+      int lin1=1, lin2=num_lin, 
+          col1=1, col2=num_col;
+      int keep_struct = IupGetInt(ih, "COPYKEEPSTRUCT");
+
+      iMatrixExCellMarkedLimits(marked, num_lin, num_col, &lin1, &lin2, &col1, &col2);
+
+      /* check consistency only when not keeping structure */
+      if (!keep_struct && !iMatrixExCellMarkedConsistent(marked, num_lin, num_col))
+      {
+        iupAttribSetStr(ih, "LASTERROR", "MARKEDCONSISTENCY");
+        iupArrayDestroy(data);
+        return;
+      }
+
+      for(lin = lin1; lin <= lin2; ++lin)
+      {
+        add_sep = 0;
+
+        if (iupMatrixExIsLineVisible(ih, lin))
+        {
+          for(col = col1; col <= col2; ++col)
+          {
+            if (iupMatrixExIsColumnVisible(ih, col))
+            {
+              if (marked[lin-1] == '1')
+              {
+                if (add_sep)
+                  iMatrixExArrayAddChar(data, '\t');
+                iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
+                add_sep = 1;
+              }
+              else if (keep_struct)
+              {
+                if (add_sep)
+                  iMatrixExArrayAddChar(data, '\t');
+                iMatrixExArrayAddEmpty(data);
+                add_sep = 1;
+              }
+            }
+          }
+        }
+
+        if (add_sep)
+          iMatrixExArrayAddChar(data, '\n');
+      }
+    }
+  }
+  else 
+  {
+    int lin1=1, lin2=num_lin, 
+        col1=1, col2=num_col;
+
+    if (!iupStrEqualNoCase(value, "ALL"))
+    {
+      char* value2 = (char*)value;
+      char* value1 = iupStrCopyUntil(&value2, '-');
+      iupStrToIntInt(value1, &lin1, &col1, ':');
+      iupStrToIntInt(value2, &lin2, &col2, ':');
+      if (value1) free(value1);
     }
 
     for(lin = lin1; lin <= lin2; ++lin)
@@ -280,20 +324,12 @@ static int iMatrixExSetCopyAttrib(Ihandle *ih, const char* value)
         {
           if (iupMatrixExIsColumnVisible(ih, col))
           {
-            if (marked[lin-1] == '1')
-            {
-              if (add_sep)
-                iMatrixExArrayAddChar(data, '\t');
-              iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
-              add_sep = 1;
-            }
-            else if (keep_struct)
-            {
-              if (add_sep)
-                iMatrixExArrayAddChar(data, '\t');
-              iMatrixExArrayAddEmpty(data);
-              add_sep = 1;
-            }
+            if (add_sep)
+              iMatrixExArrayAddChar(data, '\t');
+
+            iMatrixExArrayAddCell(ih, data, lin, col, value_cb);
+
+            add_sep = 1;
           }
         }
       }
@@ -302,18 +338,338 @@ static int iMatrixExSetCopyAttrib(Ihandle *ih, const char* value)
         iMatrixExArrayAddChar(data, '\n');
     }
   }
+}
+
+static int iMatrixExSetCopyAttrib(Ihandle *ih, const char* value)
+{
+  Iarray* data =  iupArrayCreate(100, sizeof(char));
+
+  iMatrixExSetCopyData(ih, data, value);
 
   if (iupArrayCount(data)!=0)
   {
-    Ihandle* clipboard = IupClipboard();
-    IupSetAttribute(clipboard, "TEXT", NULL);  /* clear all data from clipboard */
+    Ihandle* clipboard;
+
     iMatrixExArrayAddChar(data, '\0');
+
+    clipboard = IupClipboard();
+    IupSetAttribute(clipboard, "TEXT", NULL);  /* clear all data from clipboard */
     IupSetAttribute(clipboard, "TEXT", (char*)iupArrayGetData(data));
     IupDestroy(clipboard);
   }
 
   iupArrayDestroy(data);
+  return 0;
+}
 
+static int iMatrixExSetCopyDataAttrib(Ihandle *ih, const char* value)
+{
+  if (!value)
+    iupAttribSetStr(ih, "COPYDATA", NULL);
+  else
+  {
+    Iarray* data =  iupArrayCreate(100, sizeof(char));
+
+    iMatrixExSetCopyData(ih, data, value);
+
+    if (iupArrayCount(data)!=0)
+    {
+      iMatrixExArrayAddChar(data, '\0');
+
+      iupAttribStoreStr(ih, "COPYDATA", iupArrayGetData(data));
+    }
+
+    iupArrayDestroy(data);
+  }
+  return 0;
+}
+
+static void iMatrixExCopyNoSepTXT(char* buffer, const char* str, char sep)
+{
+  while (*str)
+  {
+    if (*str==sep || *str=='\n')
+      *buffer = ' ';
+    else
+      *buffer = *str;
+
+    buffer++;
+    str++;
+  }
+  *buffer = 0;
+}
+
+static void iMatrixExCopyNoSepHTML(char* buffer, const char* str)
+{
+  while (*str)
+  {
+    if (*str=='\n')
+    {
+      *buffer = '<'; buffer++;
+      *buffer = 'B'; buffer++;
+      *buffer = 'R'; buffer++;
+      *buffer = '>'; 
+    }
+    else
+      *buffer = *str;
+
+    buffer++;
+    str++;
+  }
+  *buffer = 0;
+}
+
+static void iMatrixExCopyNoSepLaTeX(char* buffer, const char* str)
+{
+  while (*str)
+  {
+    if (*str=='\n' || *str=='_')
+      *buffer = ' ';
+    else if (*str=='%')
+    {
+      *buffer = '\\'; buffer++;
+      *buffer = '%'; 
+    }
+    else
+      *buffer = *str;
+
+    buffer++;
+    str++;
+  }
+  *buffer = 0;
+}
+
+static void iMatrixExCopyTXT(Ihandle *ih, FILE* file, int num_lin, int num_col, char* buffer, sIFnii value_cb)
+{
+  int lin, col;
+  int add_sep;
+  char* str, sep = '\t';
+
+  str = IupGetAttribute(ih, "TEXTSEPARATOR");
+  if (str) sep = *str;
+
+  /* Here includes the title cells */
+
+  str = iupAttribGetStr(ih, "COPYCAPTION");
+  if (str)
+    fprintf(file,"%s\n",str);
+
+  for(lin = 0; lin <= num_lin; ++lin)
+  {
+    add_sep = 0;
+
+    if (iupMatrixExIsLineVisible(ih, lin))
+    {
+      for(col = 0; col <= num_col; ++col)
+      {
+        if (iupMatrixExIsColumnVisible(ih, col))
+        {
+          if (add_sep)
+            fprintf(file, "%c", sep);
+
+          str = iupMatrixExGetCell(ih, lin, col, value_cb);
+          if (str)
+          {
+            iMatrixExCopyNoSepTXT(buffer, str, sep);
+            fprintf(file, "%s", buffer);
+          }
+          else
+            fprintf(file, "%s", " ");
+
+          add_sep = 1;
+        }
+      }
+    }
+
+    if (add_sep)
+      fprintf(file, "%s", "\n");
+  }
+}
+
+static void iMatrixExCopyHTML(Ihandle *ih, FILE* file, int num_lin, int num_col, char* buffer, sIFnii value_cb)
+{
+  int lin, col;
+  char* str, *caption;
+
+  char* table = iupAttribGetStr(ih, "HTML<TABLE>");
+  char* tr = iupAttribGetStr(ih, "HTML<TR>");
+  char* th = iupAttribGetStr(ih, "HTML<TH>");
+  char* td = iupAttribGetStr(ih, "HTML<TD>");
+  caption = iupAttribGetStr(ih, "HTML<CAPTION>");
+  if (!table) table = "";
+  if (!tr) tr = "";
+  if (!th) th = "";
+  if (!td) td = "";
+  if (!caption) caption = "";
+
+  /* Here includes the title cells */
+
+  fprintf(file,"<!-- File automatically generated by IUP -->\n");
+  fprintf(file,"<TABLE%s>\n", table);
+
+  str = iupAttribGetStr(ih, "COPYCAPTION");
+  if (str)
+    fprintf(file,"<CAPTION%s>%s</CAPTION>\n", caption, str);
+
+  for(lin = 0; lin <= num_lin; ++lin)
+  {
+    if (iupMatrixExIsLineVisible(ih, lin))
+    {
+      fprintf(file,"<TR%s> ", tr);
+
+      for(col = 0; col <= num_col; ++col)
+      {
+        if (iupMatrixExIsColumnVisible(ih, col))
+        {           
+          if (lin==0||col==0)
+            fprintf(file,"<TH%s>", th);
+          else
+            fprintf(file,"<TD%s>", td);
+
+          str = iupMatrixExGetCell(ih, lin, col, value_cb);
+          if (str)
+          {
+            iMatrixExCopyNoSepHTML(buffer, str);
+            fprintf(file, "%s", buffer);
+          }
+          else
+            fprintf(file, "%s", " ");
+
+          if (lin==0||col==0)
+            fprintf(file,"</TH> ");
+          else
+            fprintf(file,"</TD> ");
+        }
+      }
+
+      fprintf(file,"</TR>\n");
+    }
+  }
+
+  fprintf(file,"</TABLE>\n");
+}
+
+static int iMatrixExIsBoldLine(Ihandle* ih, int lin)
+{
+  char str[100];
+  char* value;
+
+  if (lin==0)
+    return 0;
+
+  sprintf(str, "FONT%d:*", lin);
+  value = iupAttribGet(ih, str);
+  if (value)
+  {
+    if (strstr(value, "Bold")||strstr(value, "BOLD"))
+      return 1;
+  }
+
+  return 0;
+}
+
+static void iMatrixExCopyLaTeX(Ihandle *ih, FILE* file, int num_lin, int num_col, char* buffer, sIFnii value_cb)
+{
+  int lin, col;
+  int add_sep;
+  char* str;
+
+  /* Here includes the title cells */
+
+  fprintf(file,"%% File automatically generated by Dmatrix \n");
+
+  fprintf(file,"\\begin{table}\n");
+  fprintf(file,"\\begin{center}\n");
+  fprintf(file,"\\begin{tabular}{");
+
+  for(col = 0; col <= num_col; ++col)
+  {
+    if (iupMatrixExIsColumnVisible(ih, col))
+      fprintf(file,"|r");
+  }
+  fprintf(file,"|} \\hline\n");
+
+  for(lin = 0; lin <= num_lin; ++lin)
+  {
+    add_sep = 0;
+
+    if (iupMatrixExIsLineVisible(ih, lin))
+    {
+      int is_bold = iMatrixExIsBoldLine(ih, lin);
+
+      for(col = 0; col <= num_col; ++col)
+      {
+        if (iupMatrixExIsColumnVisible(ih, col))
+        {    
+          if (add_sep)
+            fprintf(file,"& ");
+
+          if (is_bold)
+            fprintf(file,"\\bf{");
+
+          str = iupMatrixExGetCell(ih, lin, col, value_cb);
+          if (str)
+          {
+            iMatrixExCopyNoSepLaTeX(buffer, str);
+            fprintf(file, "%s", buffer);
+          }
+          else
+            fprintf(file, "%s", " ");
+
+          if (is_bold)
+            fprintf(file,"}");
+
+          add_sep = 1;
+        }
+      }
+
+      fprintf(file,"\\\\ \\hline\n");
+    }
+  }
+
+  fprintf(file,"\\end{tabular}\n");
+
+  str = iupAttribGetStr(ih, "COPYCAPTION");
+  if (str) fprintf(file,"\\caption{%s.}\n", str);
+
+  str = iupAttribGetStr(ih, "LATEXLABEL");
+  if (str) fprintf(file,"\\label{tab:%s}\n", str);
+
+  fprintf(file,"\\end{center}\n");
+  fprintf(file,"\\end{table}\n");
+}
+
+static int iMatrixExSetCopyFileAttrib(Ihandle *ih, const char* value)
+{
+  int num_lin, 
+      num_col;
+  sIFnii value_cb;
+  char buffer[1024];
+  char* format;
+
+  FILE *file = fopen(value, "wb");
+  if (!file)
+  {
+    iupAttribSetStr(ih, "LASTERROR", "INVALIDFILENAME");
+    return 0;
+  }
+
+  /* reset error state */
+  iupAttribSetStr(ih, "LASTERROR", NULL);
+
+  num_lin = IupGetInt(ih, "NUMLIN");
+  num_col = IupGetInt(ih, "NUMCOL");
+  value_cb = (sIFnii)IupGetCallback(ih, "VALUE_CB");
+
+  format = iupAttribGetStr(ih, "TEXTFORMAT");
+  if (iupStrEqualNoCase(format, "HTML"))
+    iMatrixExCopyHTML(ih, file, num_lin, num_col, buffer, value_cb);
+  else if (iupStrEqualNoCase(format, "LaTeX"))
+    iMatrixExCopyLaTeX(ih, file, num_lin, num_col, buffer, value_cb);
+  else
+    iMatrixExCopyTXT(ih, file, num_lin, num_col, buffer, value_cb);
+
+  fclose(file);
   return 0;
 }
 
@@ -583,10 +939,29 @@ void iupMatrixExRegisterClipboard(Iclass* ic)
   iupClassRegisterAttribute(ic, "PASTEFILE", NULL, iMatrixExSetPasteFileAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NO_INHERIT);
 
   iupClassRegisterAttribute(ic, "COPY", NULL, iMatrixExSetCopyAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "COPYDATA", NULL, iMatrixExSetCopyDataAttrib, NULL, NULL, IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "COPYKEEPSTRUCT", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "COPYFILE", NULL, iMatrixExSetCopyFileAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NO_INHERIT);
+
+  iupClassRegisterAttribute(ic, "COPYCAPTION", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "LATEXLABEL", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HTML<TABLE>", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HTML<TR>", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HTML<TH>", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HTML<TD>", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "HTML<CAPTION>", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
 
   iupClassRegisterAttribute(ic, "LASTERROR", NULL, NULL, NULL, NULL, IUPAF_READONLY|IUPAF_NO_INHERIT);
 
   iupClassRegisterAttribute(ic, "TEXTSEPARATOR", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "TEXTSKIPLINES", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
 }
+
+  //COPY=ALL
+  //COPY=L1:C1-L2:C2
+  //COPYDATA
+  //COPYFILE
+  //TEXTFORMAT=TXT, HTML, LaTeX
+  //HTML*
+  //COPYCAPTION
+  //LATEXLABEL
