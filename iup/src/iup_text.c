84f9d6aaaf4acd1951fea38b2b84452102fe4c87
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2013-10-28 20:42:58 +0000

    New: MASKFAIL_CB callback for IupText and IupList when MASK is used and an invalid text is typed.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@1968 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/iup_text.c
+++ iup/src/iup_text.c
@@ -66,6 +66,8 @@ int iupEditCallActionCb(Ihandle* ih, IFnis cb, const char* insert_value, int sta
 
   if (mask && iupMaskCheck((Imask*)mask, new_value)==0)
   {
+    IFns cb = (IFns)IupGetCallback(ih, "MASKFAIL_CB");
+    if (cb) cb(ih, new_value);
     if (new_value != value) free(new_value);
     return 0; /* abort */
   }
