56711fef1baad8edc7f4a70cdb8ef8732ba172dd
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2014-05-08 21:15:23 +0000

    New: CD_IUPDBUFFER and CD_IUPDBUFFERRGB drivers in the iupcd library. IMPORTANT: This IUP version depends on CD version 5.8.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2178 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/iuplua_cd.c
+++ iup/srclua5/iuplua_cd.c
@@ -26,6 +26,26 @@ static void *cdiup_checkdata(lua_State *L, int param)
   return iuplua_checkihandle(L, param);
 }
 
+static cdluaContext cdluaiupdbufferrgbctx =
+{
+  0,
+  "IUPDBUFFERRGB",
+  cdContextIupDBufferRGB,
+  cdiup_checkdata,
+  NULL,
+  0
+};
+
+static cdluaContext cdluaiupdbufferctx =
+{
+  0,
+  "IUPDBUFFER",
+  cdContextIupDBuffer,
+  cdiup_checkdata,
+  NULL,
+  0
+};
+
 static cdluaContext cdluaiupctx = 
 {
   0,
@@ -47,6 +67,8 @@ int cdluaiup_open (lua_State *L)
   lua_gettable(L, LUA_GLOBALSINDEX);     
 #endif
   cdlua_addcontext(L, cdL, &cdluaiupctx);
+  cdlua_addcontext(L, cdL, &cdluaiupdbufferctx);
+  cdlua_addcontext(L, cdL, &cdluaiupdbufferrgbctx);
   return 1;
 }
 
