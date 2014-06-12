f65194fcea9346a1183cffdc23e88285d76ec68b
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2014-06-12 19:40:00 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2255 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/test/glcanvas_cube.c
+++ iup/test/glcanvas_cube.c
@@ -400,7 +400,7 @@ void GLCanvasCubeTest(void)
   IupSetAttribute(gvbox, "MARGIN", "5x5");
   IupSetAttribute(gvbox, "GAP", "5");
 
-  gframe1 = IupSetAttributes(IupGLFrame(ghbox), "TITLE=Frame, TITLEBOX=Yes, XXX_MOVEABLE=Yes");
+  gframe1 = IupSetAttributes(IupGLFrame(ghbox), "TITLE=Frame, TITLEBOX=Yes");
   gframe2 = IupSetAttributes(IupGLFrame(gvbox), "BACKGROUND=\"250 250 160\", FRAMECOLOR=\"250 250 160\"");
 
   canvas = IupGLCanvasBox(
