45521c1b80d8670fb07507edd171c30e149873dc
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2014-08-12 20:06:30 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2399 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/make_uname.bat
+++ iup/make_uname.bat
@@ -2,6 +2,7 @@
 REM This builds all the libraries for 1 uname
 
 FOR %%u IN (src srccd srccontrols srcmatrixex srcgl srcglcontrols srcpplot srcmglplot srcscintilla srcim srcimglib srcole srctuio srcweb srcledc srcview srclua5 srcconsole test) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 if defined TECGRAF_INTERNAL goto tec_internal
 goto end
 
