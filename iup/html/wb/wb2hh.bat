1b1574aa9c8d8553072f9f976485d6dc0b7a5261
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2014-09-01 17:18:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2478 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/wb/wb2hh.bat
+++ iup/html/wb/wb2hh.bat
@@ -1,26 +1,29 @@
 @echo off
 
+set lng=en
+REM set lng=%1
+
 Echo Building...
-lua5.1 make_hh.lua %1
+lua5.1 make_hh.lua %lng%
 Echo .
 pause
 
 Echo Preparing...
-move wb_%1.hhp ..
-move wb_tree_%1.hhc ..
+move wb_%lng%.hhp ..
+move wb_tree_%lng%.hhc ..
 cd ..
 move download download.old
 Echo .
 pause
 
 Echo Compiling...
-hhc wb_%1.hhp
+hhc wb_%lng%.hhp
 Echo .
 pause     
 
 Echo Finishing...     
-move wb_%1.hhp wb
-move wb_tree_%1.hhc wb
+move wb_%lng%.hhp wb
+move wb_tree_%lng%.hhc wb
 move download.old download
 move /y *.chm download
 cd wb
