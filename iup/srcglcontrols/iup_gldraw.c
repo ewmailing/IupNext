af235fc23064e45411445ab52906ab24a7139540
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2014-09-15 16:16:43 +0000

    changed filter to GL_NEAREST
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2503 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcglcontrols/iup_gldraw.c
+++ iup/srcglcontrols/iup_gldraw.c
@@ -421,8 +421,8 @@ static GLuint iGLDrawGenTexture(Ihandle* ih, Ihandle* image, int active)
 
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
     glTexImage2D(GL_TEXTURE_2D, 0, format, image->currentwidth, image->currentheight, 0, format, GL_UNSIGNED_BYTE, gldata);
 
