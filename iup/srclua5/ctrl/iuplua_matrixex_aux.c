9f7243b239f1b803f9891d594005744c43330e19
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-01-07 19:59:32 +0000

    new IupMatrixExSetDynamic (not working, still needs IupMatrix changes)
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2683 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/ctrl/iuplua_matrixex_aux.c
+++ iup/srclua5/ctrl/iuplua_matrixex_aux.c
@@ -22,6 +22,7 @@
 
 
 typedef int(*IFnL)(Ihandle*, lua_State *L);
+typedef char* (*sIFniis)(Ihandle*, int, int, char*);
 
 
 static int math_sum(lua_State *L) 
@@ -132,7 +133,7 @@ static int formula_ifelse(lua_State *L)
   return 1;
 }
 
-static void ShowFormulaError(Ihandle* ih, lua_State *L)
+static void iMatrixExShowFormulaError(Ihandle* ih, lua_State *L)
 {
   const char* str_message = IupGetLanguageString("IUP_ERRORINVALIDFORMULA");
   const char* error = lua_tostring(L, -1);
@@ -141,21 +142,19 @@ static void ShowFormulaError(Ihandle* ih, lua_State *L)
   iupShowError(IupGetDialog(ih), msg);
 }
 
-void IupMatrixExSetFormula(Ihandle* ih, int col, const char* formula, const char* init)
+static lua_State* iMatrixExInitFormula(Ihandle* ih, const char* init)
 {
   lua_State *L;
-  int lin, numlin;
-  char formula_func[1024];
   IFnL init_cb;
 
   iupASSERT(iupObjectCheck(ih));
   if (!iupObjectCheck(ih))
-    return;
+    return NULL;
 
   /* must be an IupMatrix */
   if (ih->iclass->nativetype != IUP_TYPECANVAS ||
       !IupClassMatch(ih, "matrix"))
-      return;
+    return NULL;
 
   L = luaL_newstate();
   luaL_openlibs(L);
@@ -182,34 +181,65 @@ void IupMatrixExSetFormula(Ihandle* ih, int col, const char* formula, const char
   if (init_cb)
     init_cb(ih, L);
 
-  numlin = IupGetInt(ih, "NUMLIN");
-
   lua_pushlightuserdata(L, ih);
   lua_setglobal(L, "matrix");
 
+  return L;
+}
+
+static int iMatrixExLoadFormula(lua_State *L, const char* formula)
+{
+  char formula_func[1024];
+
   sprintf(formula_func, "function matrix_formula(lin, col)\n"
-                        "  return %s\n"
-                        "end\n", formula);
+          "  return %s\n"
+          "end\n", formula);
 
   if (luaL_dostring(L, formula_func) != 0)
+    return 0;
+
+  return 1;
+}
+
+static int iMatrixExExecFormula(lua_State *L, int lin, int col)
+{
+  lua_getglobal(L, "matrix_formula");
+  lua_pushinteger(L, lin);
+  lua_pushinteger(L, col);
+
+  if (lua_pcall(L, 2, 1, 0) != 0)
+    return 0;
+
+  return 1;
+}
+
+void IupMatrixExSetFormula(Ihandle* ih, int col, const char* formula, const char* init)
+{
+  int lin, numlin;
+
+  lua_State *L = iMatrixExInitFormula(ih, init);
+  if (!L)
+    return;
+
+  if (!iMatrixExLoadFormula(L, formula))
   {
-    ShowFormulaError(ih, L);
+    iMatrixExShowFormulaError(ih, L);
     lua_close(L);
     return;
   }
 
+  numlin = IupGetInt(ih, "NUMLIN");
+
   for (lin = 1; lin <= numlin; lin++)
   {
-    lua_getglobal(L, "matrix_formula");
-    lua_pushinteger(L, lin);
-    lua_pushinteger(L, col);
-
-    if (lua_pcall(L, 2, 1, 0) != 0)
+    if (!iMatrixExExecFormula(L, lin, col))
     {
-      ShowFormulaError(ih, L);
+      iMatrixExShowFormulaError(ih, L);
       lua_close(L);
       return;
     }
+
+    /* get the result */
 
     if (lua_isnumber(L, -1))
     {
@@ -235,132 +265,82 @@ void IupMatrixExSetFormula(Ihandle* ih, int col, const char* formula, const char
   lua_close(L);
 }
 
-#if 0
-
-  double DynamicColumn::GetValue(lua_State *L) const
-  {
-    int top1 = lua_gettop(L);
-    luaL_dostring(L, macro.c_str());
-    int top2 = lua_gettop(L);
-    if (top2 - top1 == 1 && lua_isnumber(L, -1))
-      return lua_tonumber(L, -1);
-    return 0.0;
-  }
-}
-
-int Dmatrix::Lua_callback_cell(lua_State *L)
+static char* iMatrixExDynamicTranslate_CB(Ihandle* ih, int lin, int col, char* value)
 {
-  Dmatrix *D = current_dmatrix;
-  if (lua_gettop(L) < 1)
+  if (value && value[0] == '=')
   {
-    lua_pushstring(L, "Error in script! Function cell() expected one or two numbers as parameters.");
-    lua_error(L);
-    return 0;
-  }
-  int lin = D->Lmanager.curr_lin;
-  int col = D->Lmanager.curr_col;
-  if (lua_gettop(L) <= 2)
-  {
-    if (lua_type(L, -2) == LUA_TNUMBER)
-      col = static_cast<int>(lua_tonumber(L, -1));
-    else
+    lua_State* L = (lua_State*)iupAttribGet(ih, "_IUPMATRIXEX_LUASTATE");
+
+    if (!iMatrixExLoadFormula(L, value+1))
     {
-      lua_pushstring(L, "Error in script! Function cell() expected one or two numbers as parameters.");
-      lua_error(L);
-      return 0;
+      const char* str_message = IupGetLanguageString("IUP_ERRORINVALIDFORMULA");
+      return (char*)str_message;
     }
-  }
-  if (lua_type(L, -1) == LUA_TNUMBER)
-    lin = static_cast<int>(lua_tonumber(L, -2));
-  else
-  {
-    lua_pushstring(L, "Error in script! Function cell() expected one or two numbers as parameters.");
-    lua_error(L);
-    return 0;
+
+    if (!iMatrixExExecFormula(L, lin, col))
+    {
+      const char* str_message = IupGetLanguageString("IUP_ERRORINVALIDFORMULA");
+      return (char*)str_message;
+    }
+
+    /* get the result */
+
+    if (lua_isnumber(L, -1))
+    {
+      double num = lua_tonumber(L, -1);
+
+    }
+
+    value = (char*)lua_tostring(L, -1);
+    iupAttribSetStr(ih, "FORMULA_RETURN", value);
+
+    lua_pop(L, 1);  /* remove the result from the stack */
+    return iupAttribGet(ih, "FORMULA_RETURN");
   }
 
-  if (col == D->Lmanager.curr_col)
-  {
-    lua_pushstring(L, "Error in script! Circular reference found!");
-    lua_error(L);
-    return 0;
-  }
-
-  std::string s = D->GetValue(lin, col);
-  if (s.empty())
-  {
-    lua_pushnil(L);
-    return 1;
-  }
-  double num(0.0);
-  if (sscanf(s.c_str(), "%lf", &num) == 1)
-  {
-    lua_pushnumber(L, num);
-    return 1;
-  }
-  else
-  {
-    s.insert('\"', 0);
-    s.append("\"");
-    lua_pushstring(L, s.c_str());
-    return 1;
-  }
-  lua_pushstring(L, "Error in script! Function cell() expected one or two numbers as parameters.");
-  lua_error(L);
-  return 0;
+  return value;
 }
 
-char *Dmatrix::GetValueLua(Column *C, int lin, int col)
+static int iMatrixExLDestroy_CB(Ihandle* ih)
 {
-  if (lin == 0)
-    return (char*)C->macro.c_str();
-  bool was_running = Lmanager.running;
-  if (C->macro.empty() || !C->macro_ok)
-    return "nil";
-  if (!Lmanager.running)
-  {
-    current_dmatrix = this;
-    Lmanager.running = true;
-    Lmanager.curr_lin = lin;
-    Lmanager.curr_col = col;
-  }
-  //luaL_dostring(Lmanager.L,C->macro.c_str());
-  std::string e;
-  std::string macro = "dmatrix_variable =";
-  macro.append(C->macro);
-  if ((luaL_loadstring(Lmanager.L, macro.c_str())) != 0)
-  {
-    e = lua_tostring(Lmanager.L, -1);
-    e.append("\n");
-    //e.append(C->macro_ok?"true":"false");
-    //printf(e.c_str());
-    C->macro_ok = false;
-    IupMessage("Dmatrix Lua Alert!", e.c_str());
-    if (!was_running) Lmanager.running = false;
-    return "nil";
-  }
-  if ((lua_pcall(Lmanager.L, 0, 0, 0)) != 0)
-  {
-    e = lua_tostring(Lmanager.L, -1);
-    e.append("\n");
-    //printf(e.c_str());
-    C->macro_ok = false;
-    IupMessage("Dmatrix Lua Alert!", e.c_str());
-    if (!was_running) Lmanager.running = false;
-    return "nil";
-  }
-  lua_getglobal(Lmanager.L, "dmatrix_variable");
-  if (!was_running)
-    Lmanager.running = false;
-  return (char*)lua_tostring(Lmanager.L, -1);
+  Icallback cb = IupGetCallback(ih, "OLD_LDESTROY_CB");
+  lua_State* L = (lua_State*)iupAttribGet(ih, "_IUPMATRIXEX_LUASTATE");
+  IupSetCallback(ih, "TRANSLATE_CB", NULL);
+  lua_close(L);
+  if (cb)
+    cb(ih);
+  return IUP_DEFAULT;
 }
 
-#endif
+void IupMatrixExSetDynamic(Ihandle* ih, const char* init)
+{
+  Icallback cb;
+
+  lua_State *L = iMatrixExInitFormula(ih, init);
+  if (!L)
+    return;
+
+  iupAttribSet(ih, "_IUPMATRIXEX_LUASTATE", (char*)L);
+
+  cb = IupGetCallback(ih, "LDESTROY_CB");
+  if (cb)
+    IupSetCallback(ih, "OLD_LDESTROY_CB", cb);
+  IupSetCallback(ih, "LDESTROY_CB", iMatrixExLDestroy_CB);
+
+  IupSetCallback(ih, "TRANSLATE_CB", (Icallback)iMatrixExDynamicTranslate_CB);
+}
 
 static int MatrixExSetFormula(lua_State *L)
 {
   Ihandle *ih = iuplua_checkihandle(L,1);
   IupMatrixExSetFormula(ih, luaL_checkint(L, 2), luaL_checkstring(L, 3), luaL_optstring(L, 4, NULL));
+  return 0;
+}
+
+static int MatrixExSetDynamic(lua_State *L)
+{
+  Ihandle *ih = iuplua_checkihandle(L, 1);
+  IupMatrixExSetDynamic(ih, luaL_optstring(L, 2, NULL));
   return 0;
 }
 
@@ -375,6 +355,7 @@ void iuplua_matrixexfuncs_open (lua_State *L)
 {
   iuplua_register(L, MatrixExInit, "MatrixExInit");
   iuplua_register(L, MatrixExSetFormula, "MatrixExSetFormula");
+  iuplua_register(L, MatrixExSetDynamic, "MatrixExSetDynamic");
 }
 
 int iup_matrixexlua_open(lua_State * L);
