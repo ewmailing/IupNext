15e41c6370ebbecf51b77172a6d3451829f42d01
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-02-04 17:14:11 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@2762 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcconsole/make_uname.bat
+++ iup/srcconsole/make_uname.bat
@@ -12,7 +12,7 @@ REM Must use dll* so USE_* will use the correct TEC_UNAME
 :iupexe32
 call tecmake dll10 "USE_LUA51=Yes" relink %2 %3 %4 %5 %6 %7
 call tecmake dll10 "USE_LUA52=Yes" relink %2 %3 %4 %5 %6 %7
-call tecmake dll10 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll10 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
 if not defined TECGRAF_INTERNAL goto end
 call tecmake vc10 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
@@ -20,7 +20,7 @@ goto end
 :iupexe64
 call tecmake dll10_64 "USE_LUA51=Yes" relink %2 %3 %4 %5 %6 %7
 call tecmake dll10_64 "USE_LUA52=Yes" relink %2 %3 %4 %5 %6 %7
-call tecmake dll10_64 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
+REM call tecmake dll10_64 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
 if not defined TECGRAF_INTERNAL goto end
 call tecmake vc10_64 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
@@ -28,7 +28,7 @@ goto end
 :iupexecygw
 call tecmake cygw17 "USE_LUA51=Yes" relink %2 %3 %4 %5 %6 %7
 call tecmake cygw17 "USE_LUA52=Yes" relink %2 %3 %4 %5 %6 %7
-call tecmake cygw17 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
+REM call tecmake cygw17 "USE_LUA53=Yes" relink %2 %3 %4 %5 %6 %7
 if not defined TECGRAF_INTERNAL goto end
 call tecmake cygw17 "MF=iuplua3" relink %2 %3 %4 %5 %6 %7
 goto end
