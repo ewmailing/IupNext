fa4cecaefa0c6ef87fa947084ee35d04ca1432f8
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-07-04 18:24:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3025 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/iuplua_config.c
+++ iup/srclua5/iuplua_config.c
@@ -17,15 +17,37 @@
 #include "il.h"
 
 
+static int ConfigLoad(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  int ret = IupConfigLoad(ih);
+  lua_pushinteger(L, ret);
+  return 1;
+}
+
+static int ConfigSave(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  int ret = IupConfigSave(ih);
+  lua_pushinteger(L, ret);
+  return 1;
+}
+
 static int config_recent_cb(Ihandle *self)
 {
   lua_State *L = iuplua_call_start(self, "recent_cb");
   return iuplua_call(L, 0);
 }
 
-void IupConfigRecentInit(Ihandle* ih, Ihandle* menu, Icallback recent_cb, int max_recent);
-int IupConfigLoad(Ihandle* ih);
-int IupConfigSave(Ihandle* ih);
+static int ConfigRecentInit(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  Ihandle* menu = iuplua_checkihandle(L, 2);
+  int max_recent = (int)luaL_checkinteger(L, 3);
+
+  IupConfigRecentInit(ih, menu, config_recent_cb, max_recent);
+  return 0;
+}
 
 static int ConfigRecentUpdate(lua_State *L)
 {
@@ -57,7 +79,12 @@ void iupconfiglua_open(lua_State * L)
 {
   iuplua_register_cb(L, "RECENT_CB", (lua_CFunction)config_recent_cb, NULL);
 
+  iuplua_register(L, ConfigLoad, "ConfigLoad");
+  iuplua_register(L, ConfigSave, "ConfigSave");
+
+  iuplua_register(L, ConfigRecentInit, "ConfigRecentInit");
+  iuplua_register(L, ConfigRecentUpdate, "ConfigRecentUpdate");
+
   iuplua_register(L, ConfigDialogShow, "ConfigDialogShow");
   iuplua_register(L, ConfigDialogClosed, "ConfigDialogClosed");
-  iuplua_register(L, ConfigRecentUpdate, "ConfigRecentUpdate");
 }
