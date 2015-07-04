9ecdfcf083b9022232cfd9adbbffe75338a4813d
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-07-04 19:04:28 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3029 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srclua5/iuplua_config.c
+++ iup/srclua5/iuplua_config.c
@@ -17,6 +17,13 @@
 #include "il.h"
 
 
+static int Config(lua_State *L)
+{
+  Ihandle* ih = IupConfig();
+  iuplua_pushihandle(L, ih);
+  return 1;
+}
+
 static int ConfigLoad(lua_State *L)
 {
   Ihandle* ih = iuplua_checkihandle(L, 1);
@@ -30,6 +37,93 @@ static int ConfigSave(lua_State *L)
   Ihandle* ih = iuplua_checkihandle(L, 1);
   int ret = IupConfigSave(ih);
   lua_pushinteger(L, ret);
+  return 1;
+}
+
+static int ConfigSetVariable(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  if (lua_isnil(L, 4))
+    IupConfigSetVariableStr(ih, group, key, NULL);
+  else
+  {
+    const char *value = luaL_checkstring(L, 4);
+    IupConfigSetVariableStr(ih, group, key, value);
+  }
+  return 0;
+}
+
+static int ConfigSetVariableId(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  int id = (int)luaL_checkinteger(L, 4);
+  if (lua_isnil(L, 5))
+    IupConfigSetVariableStrId(ih, group, key, id, NULL);
+  else
+  {
+    const char *value = luaL_checkstring(L, 5);
+    IupConfigSetVariableStrId(ih, group, key, id, value);
+  }
+  return 0;
+}
+
+static int ConfigGetVariable(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  const char* value = IupConfigGetVariableStr(ih, group, key);
+  if (!value)
+    lua_pushnil(L);
+  else
+    lua_pushstring(L, value);
+  return 1;
+}
+
+static int ConfigGetVariableId(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  int id = (int)luaL_checkinteger(L, 4);
+  const char* value = IupConfigGetVariableStrId(ih, group, key, id);
+  if (!value)
+    lua_pushnil(L);
+  else
+    lua_pushstring(L, value);
+  return 1;
+}
+
+static int ConfigGetVariableDef(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  const char *def = luaL_optstring(L, 4, NULL);
+  const char* value = IupConfigGetVariableStrDef(ih, group, key, def);
+  if (!value)
+    lua_pushnil(L);
+  else
+    lua_pushstring(L, value);
+  return 1;
+}
+
+static int ConfigGetVariableIdDef(lua_State *L)
+{
+  Ihandle* ih = iuplua_checkihandle(L, 1);
+  const char *group = luaL_checkstring(L, 2);
+  const char *key = luaL_checkstring(L, 3);
+  int id = (int)luaL_checkinteger(L, 4);
+  const char *def = luaL_optstring(L, 4, NULL);
+  const char* value = IupConfigGetVariableStrIdDef(ih, group, key, id, def);
+  if (!value)
+    lua_pushnil(L);
+  else
+    lua_pushstring(L, value);
   return 1;
 }
 
@@ -78,6 +172,15 @@ static int ConfigDialogClosed(lua_State *L)
 void iupconfiglua_open(lua_State * L)
 {
   iuplua_register_cb(L, "RECENT_CB", (lua_CFunction)config_recent_cb, NULL);
+
+  iuplua_register(L, Config, "Config");
+
+  iuplua_register(L, ConfigSetVariable, "ConfigSetVariable");
+  iuplua_register(L, ConfigSetVariableId, "ConfigSetVariableId");
+  iuplua_register(L, ConfigGetVariable, "ConfigGetVariable");
+  iuplua_register(L, ConfigGetVariableId, "ConfigGetVariableId");
+  iuplua_register(L, ConfigGetVariableDef, "ConfigGetVariableDef");
+  iuplua_register(L, ConfigGetVariableIdDef, "ConfigGetVariableIdDef");
 
   iuplua_register(L, ConfigLoad, "ConfigLoad");
   iuplua_register(L, ConfigSave, "ConfigSave");
