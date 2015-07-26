1e21363d04be8c5c8cbb818023e589ce67df720e
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-07-26 05:35:05 +0000

    naturalsize and usersize must return NULL if both values are 0
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3119 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/iup_classbase.c
+++ iup/src/iup_classbase.c
@@ -95,6 +95,8 @@ char* iupBaseGetSizeAttrib(Ihandle* ih)
   if (charwidth == 0 || charheight == 0)
     return NULL;  /* if font failed get from the hash table */
 
+  if (width == 0 && height == 0)
+    return NULL;
   return iupStrReturnIntInt(iupRASTER2WIDTH(width, charwidth), iupRASTER2HEIGHT(height, charheight), 'x');
 }
 
@@ -165,7 +167,7 @@ char* iupBaseGetCharSizeAttrib(Ihandle* ih)
   int charwidth, charheight;
 
   iupdrvFontGetCharSize(ih, &charwidth, &charheight);
-  if (charwidth == 0 || charheight == 0)
+  if (charwidth == 0 || charheight == 0)  /* if font failed get from the hash table */
     return NULL;
 
   return iupStrReturnIntInt(charwidth, charheight, 'x');
@@ -173,11 +175,15 @@ char* iupBaseGetCharSizeAttrib(Ihandle* ih)
 
 static char* iBaseGetNaturalSizeAttrib(Ihandle* ih)
 {
+  if (ih->naturalwidth == 0 && ih->naturalheight == 0)
+    return NULL;
   return iupStrReturnIntInt(ih->naturalwidth, ih->naturalheight, 'x');
 }
 
 static char* iBaseGetUserSizeAttrib(Ihandle* ih)
 {
+  if (ih->userwidth == 0 && ih->userheight == 0)
+    return NULL;
   return iupStrReturnIntInt(ih->userwidth, ih->userheight, 'x');
 }
 
