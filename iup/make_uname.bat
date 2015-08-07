80e3ffdea0029eea74ae9def2c50cb3f00ae1263
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-08-07 19:44:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3199 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/make_uname.bat
+++ iup/make_uname.bat
@@ -1,7 +1,7 @@
 @echo off
 REM This builds all the libraries for 1 uname
 
-FOR %%u IN (src srccd srccontrols srcmatrixex srcgl srcglcontrols srcplot srcpplot srcmglplot srcscintilla srcim srcimglib srcole srctuio srcweb srcledc srcview test) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9
+FOR %%u IN (src srccd srccontrols srcmatrixex srcgl srcglcontrols srcplot srcpplot srcmglplot srcscintilla srcim srcimglib srcole srctuio srcweb srcledc srcview) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
@@ -20,3 +20,5 @@ REM set USE_LUA53=Yes
 REM call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM set USE_LUA53=
+
+call make_uname_lib.bat html\examples\tests %1 %2 %3 %4 %5 %6 %7 %8 %9
