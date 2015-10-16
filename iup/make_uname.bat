e815cdb64bc4e33f77bfbb9fcfa4bd38bcd21169
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-10-16 17:59:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3397 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/make_uname.bat
+++ iup/make_uname.bat
@@ -6,20 +6,20 @@ FOR %%u IN (src srccd srccontrols srcmatrixex srcgl srcglcontrols srcplot srcppl
 call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
 
-set USE_LUA51=Yes
+REM set USE_LUA51=Yes
 REM call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
-set USE_LUA51=
+REM set USE_LUA51=
 
-set USE_LUA52=Yes
+REM set USE_LUA52=Yes
 REM call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
-set USE_LUA52=
+REM set USE_LUA52=
 
-set USE_LUA53=Yes
+REM set USE_LUA53=Yes
 REM call make_uname_lib.bat srclua5 %1 %2 %3 %4 %5 %6 %7 %8 %9
 REM call make_uname_lib.bat srcconsole %1 %2 %3 %4 %5 %6 %7 %8 %9
-set USE_LUA53=
+REM set USE_LUA53=
 
 call make_uname_lib.bat html\examples\tests %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..\..
