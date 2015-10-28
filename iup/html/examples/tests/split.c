7efaaf2456e4d8e154e41f584a9bfe0fe668614f
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2015-10-28 19:57:59 +0000

    Fixed: AUTOHIDE behavior in IupSplit.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3419 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/tests/split.c
+++ iup/html/examples/tests/split.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include "iup.h"
 
+int action(Ihandle* bt)
+{
+  IupSetAttribute(IupGetParent(bt), "VALUE", "0");
+  return IUP_DEFAULT;
+}
+
 void SplitTest(void)
 {
   Ihandle *dlg, *bt, *split, *ml, *vbox;
 
   bt = IupButton("Button", NULL);
   IupSetAttribute(bt, "EXPAND", "YES");
-  IupSetAttribute(bt, "MINSIZE", "30x");
+//  IupSetAttribute(bt, "MINSIZE", "30x");
 //  IupSetAttribute(bt, "RASTERSIZE", "30x");
+  IupSetCallback(bt, "ACTION", action);
 
   ml = IupMultiLine(NULL);
   IupSetAttribute(ml, "EXPAND", "YES");
   IupSetAttribute(ml, "VISIBLELINES", "5");
   IupSetAttribute(ml, "VISIBLECOLUMNS", "10");
   IupSetAttribute(ml, "VALUE", "Multiline");
-  IupSetAttribute(ml, "MINSIZE", "30x");
+//  IupSetAttribute(ml, "MINSIZE", "30x");
   
   split = IupSplit(bt, ml);
   //split = IupSplit(NULL, NULL);
-  //  IupSetAttribute(split, "ORIENTATION", "VERTICAL");
+  IupSetAttribute(split, "ORIENTATION", "VERTICAL");
 //  IupSetAttribute(split, "ORIENTATION", "HORIZONTAL");
 //  IupSetAttribute(split, "COLOR", "0 255 0");
 //  IupSetAttribute(split, "LAYOUTDRAG", "NO");
-//  IupSetAttribute(split, "AUTOHIDE", "YES");
+  IupSetAttribute(split, "AUTOHIDE", "YES");
 //  IupSetAttribute(split, "SHOWGRIP", "NO");
 //  IupSetAttribute(split, "MINMAX", "100:800");
 
