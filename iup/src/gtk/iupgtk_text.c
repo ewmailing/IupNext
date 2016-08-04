067586eeba894697ad0d358eef732dae33338963
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2016-08-04 18:48:19 +0000

    Changed: CUEBANNER attribute support for IupText in GTK (needs GTK version 3.2+).
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3746 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/gtk/iupgtk_text.c
+++ iup/src/gtk/iupgtk_text.c
@@ -1158,6 +1158,18 @@ static int gtkTextSetTabSizeAttrib(Ihandle* ih, const char* value)
   return 1;
 }
 
+static int gtkTextSetCueBannerAttrib(Ihandle *ih, const char *value)
+{
+  if (!ih->data->is_multiline)
+  {
+#if GTK_CHECK_VERSION(3, 2, 0)
+    gtk_entry_set_placeholder_text(GTK_ENTRY(ih->handle), iupgtkStrConvertToSystem(value));
+    return 1;
+#endif
+  }
+  return 0;
+}
+
 static int gtkTextSetOverwriteAttrib(Ihandle* ih, const char* value)
 {
   if (!ih->data->is_multiline)
@@ -1755,8 +1767,8 @@ void iupdrvTextInitClass(Iclass* ic)
   iupClassRegisterAttribute(ic, "REMOVEFORMATTING", NULL, gtkTextSetRemoveFormattingAttrib, NULL, NULL, IUPAF_WRITEONLY|IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "TABSIZE", NULL, gtkTextSetTabSizeAttrib, "8", NULL, IUPAF_DEFAULT);  /* force new default value */
   iupClassRegisterAttribute(ic, "PASSWORD", NULL, NULL, NULL, NULL, IUPAF_NO_INHERIT);
+  iupClassRegisterAttribute(ic, "CUEBANNER", NULL, gtkTextSetCueBannerAttrib, NULL, NULL, IUPAF_NO_INHERIT);
 
   /* Not Supported */
-  iupClassRegisterAttribute(ic, "CUEBANNER", NULL, NULL, NULL, NULL, IUPAF_NOT_SUPPORTED|IUPAF_NO_INHERIT);
   iupClassRegisterAttribute(ic, "FILTER", NULL, NULL, NULL, NULL, IUPAF_NOT_SUPPORTED|IUPAF_NO_INHERIT);
 }
