3e2c4b522a37dc881328c611ff7f3e2724856168
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2016-10-13 14:37:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@3838 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/CPP/alarm.cpp
+++ iup/html/examples/CPP/alarm.cpp
@@ -2,9 +2,6 @@
    Shows a dialog similar to the one shown when you exit a program without saving.
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-
 /* IUP libraries include */
 #include <iup_plus.h>
 
@@ -12,28 +9,28 @@
 int main(int argc, char **argv)
 {
   /* Initializes IUP */
-  iup::Open(argc, argv);
+  Iup::Open(argc, argv);
 
   /* Executes IupAlarm */
-  switch (iup::Alarm("IupAlarm Example", "File not saved! Save it now?", "Yes", "No", "Cancel"))
+  switch (Iup::Alarm("IupAlarm Example", "File not saved! Save it now?", "Yes", "No", "Cancel"))
   {
     /* Shows a message for each selected button */
     case 1:
-      iup::Message("Save file", "File saved successfully - leaving program");
+      Iup::Message("Save file", "File saved successfully - leaving program");
     break;
 
     case 2:
-      iup::Message("Save file", "File not saved - leaving program anyway");
+      Iup::Message("Save file", "File not saved - leaving program anyway");
     break;
 
     case 3:
-      iup::Message("Save file", "Operation canceled");
+      Iup::Message("Save file", "Operation canceled");
     break;
   }
   
   /* Finishes IUP */
-  iup::Close();
+  Iup::Close();
 
   /* Program finished successfully */
-  return EXIT_SUCCESS;
+  return 0;
 }
