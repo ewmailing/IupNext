aa9daf8cb7dd965f47077254180370fd03f5e57d
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2017-04-09 23:53:09 +0000

    Fixed invert axis
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4059 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srccontrols/matrix/iupmat_def.h
+++ iup/srccontrols/matrix/iupmat_def.h
@@ -172,7 +172,7 @@ struct _IcontrolData
 int iupMatrixIsValid(Ihandle* ih, int check_cells);
 void iupMatrixRegisterEx(Iclass* ic);
 
-#define iupMATRIX_INVERTYAXIS(_ih, _y) ((_ih)->data->h-1 - (_y))
+#define iupMATRIX_INVERTYAXIS(_ih, _y) cdCanvasInvertYAxis((_ih)->data->cd_canvas, (_y))
 
 #define iupMATRIX_CHECK_COL(_ih, _col) ((_col >= 0) && (_col < (_ih)->data->columns.num))
 #define iupMATRIX_CHECK_LIN(_ih, _lin) ((_lin >= 0) && (_lin < (_ih)->data->lines.num))
