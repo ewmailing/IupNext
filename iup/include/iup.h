a3b5f75eec00bfba8f2fbebc5a93b1cdd22b1b8a
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2018-01-19 13:18:12 +0000

    copyright 2018
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4639 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/include/iup.h
+++ iup/include/iup.h
@@ -463,7 +463,7 @@ int IupMain (int argc, char** argv); /* In C++ we have to declare the prototype 
 #endif
 
 /******************************************************************************
-* Copyright (C) 1994-2017 Tecgraf/PUC-Rio.
+* Copyright (C) 1994-2018 Tecgraf/PUC-Rio.
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
