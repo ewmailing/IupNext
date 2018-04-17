383aa5f6799358390176706986302b47c22f7a24
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2018-04-17 22:01:15 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4803 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup2/make_uname.bat
+++ iup2/make_uname.bat
@@ -1,4 +1,4 @@
 @echo off
 REM This builds all the libraries for 1 uname
                                   
-FOR %%u IN (src srccd srccontrols srcpplot srcgl srcim srcole srcledc srcview srclua3 srclua5 srcconsole) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9                                  
+FOR %%u IN (src srccd srccontrols srcpplot srcgl srcim srcole srcledc srcview srclua5 srcconsole) DO call make_uname_lib.bat %%u %1 %2 %3 %4 %5 %6 %7 %8 %9                                  
