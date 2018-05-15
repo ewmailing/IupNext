c156080aa93bf84a2ce5d383545a8f00f59e3c41
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2018-05-15 17:57:17 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4889 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/tests/matrix.c
+++ iup/html/examples/tests/matrix.c
@@ -228,7 +228,7 @@ static Ihandle* create_matrix(void)
   IupSetAttribute(mat, "NUMLIN_VISIBLE", "5");
 //  IupSetAttribute(mat,"EDITNEXT","COLCR");
 
-  IupSetAttribute(mat, "COLRESDRAG", "Yes");
+  IupSetAttribute(mat, "RESIZEDRAG", "Yes");
 
   IupSetAttribute(mat, "NUMCOL_NOSCROLL", "1");
   IupSetAttribute(mat, "NOSCROLLASTITLE", "Yes");
