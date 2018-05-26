92f67c58b34377a629ca30e6de8fbf8172434b49
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2018-05-26 21:35:07 +0000

    Preparing 3.25
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4912 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/include/iup.h
+++ iup/include/iup.h
@@ -21,9 +21,9 @@ extern "C" {
 #define IUP_NAME "IUP - Portable User Interface"
 #define IUP_DESCRIPTION	"Multi-platform Toolkit for Building Graphical User Interfaces"
 #define IUP_COPYRIGHT "Copyright (C) 1994-2018 Tecgraf/PUC-Rio"
-#define IUP_VERSION "3.24"         /* bug fixes are reported only by IupVersion functions */
-#define IUP_VERSION_NUMBER 324000
-#define IUP_VERSION_DATE "2018/01/22"  /* does not include bug fix releases */
+#define IUP_VERSION "3.25"         /* bug fixes are reported only by IupVersion functions */
+#define IUP_VERSION_NUMBER 325000
+#define IUP_VERSION_DATE "2018/05/28"  /* does not include bug fix releases */
 
 typedef struct Ihandle_ Ihandle;
 typedef int (*Icallback)(Ihandle*);
