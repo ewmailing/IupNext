fe628ffea510cda44d21e34cf6e62bb9488d256c
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2018-05-30 21:11:34 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@4919 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/Lua/plot.lua
+++ iup/html/examples/Lua/plot.lua
@@ -16,6 +16,16 @@ plot:Add(0, 0)
 plot:Add(1, 1)
 plot:End()
 
+function plot:predraw_cb(canvas)
+  canvas:Foreground(cd.EncodeColor(0, 0, 255))
+  canvas:Rect(200, 700, 200, 700) -- pixels
+  canvas:Line(200, 700, 700, 200) -- pixels
+
+  local x1, y1 = iup.PlotTransform(plot, 1, 0.9) -- plot coordinates
+  local x2, y2 = iup.PlotTransform(plot, 1, 0.1)
+  canvas:Line(x1, y1, x1, y2) -- pixels
+end
+
 dlg = iup.dialog{
     TITLE = "Plot Test",
     SIZE = "400x200",
