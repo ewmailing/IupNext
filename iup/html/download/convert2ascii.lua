420016938797fc96cd474ff8e5741102772160a9
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2019-01-06 16:35:05 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@5082 2f08bc20-984a-4df8-b195-9b5d5d477537

---
+++ iup/html/download/convert2ascii.lua
@@ -0,0 +1,51 @@
+-- Converts file encoding to pure ASCii
+-- Including Comments
+-- For instance: "Atenção!" would be "Aten\xE7\xE3o!". 
+-- But actually will be "Aten\xE7""\xE3""o!" to properly encapsulate the hexadecinal representation
+-- Solves the problem of some compilers when detecting the file encoding
+-- Based on a contribution form Could Wu
+-- Run this from the iup base folder
+-- It will replace the original files
+-- Works on Lua 5.2 or 5.3 (not working on 5.1, must check gsub behavior)
+-- Usage: iup> lua convert2ascii.lua
+
+local function toascii(c)
+  local ascii = string.byte(c)
+  local hex = string.format("\\x%X", ascii)
+  return hex .. '"' .. ( c:sub(-1) == '"' and "" or '"')
+end
+
+function toascii_direct(c)
+  local ascii = string.byte(c)
+  return ascii
+end
+
+local function convert(filename, direct)
+  print(filename)
+	local f = assert(io.open(filename,"rb"))
+	local text = f:read("*a")
+	f:close()
+
+  if (direct) then
+    text = text:gsub("'([\x80-\xff])'", toascii_direct)
+  else
+	  text = text:gsub('[\x80-\xff]"?', toascii) -- from 128 to 255
+  end
+  
+	f = assert(io.open(filename, "wb"))
+	f:write(text)
+	f:close()
+end
+
+print("Converting to ASCii:\n")
+
+convert("src/iup_str.c", true) -- Uses a different conversion
+convert("src/iup_strmessage.c")
+convert("srccontrols/matrix/iupmatrix.c")
+convert("srccontrols/matrixex/iupmatex_find.c")
+convert("srccontrols/matrixex/iupmatex_sort.c")
+convert("srccontrols/matrixex/iupmatex_units.c")
+convert("srccontrols/matrixex/iup_matrixex.c")
+convert("srcplot/iup_plot.cpp")
+
+print("\nDone")
