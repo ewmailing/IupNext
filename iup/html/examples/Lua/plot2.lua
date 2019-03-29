3a9084298d627ac85bc297cb4325858e3a8f7387
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2019-03-29 00:49:42 +0000

    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@5129 2f08bc20-984a-4df8-b195-9b5d5d477537

---
+++ iup/html/examples/Lua/plot2.lua
@@ -0,0 +1,31 @@
+
+require("iuplua")
+require("iuplua_plot")
+require("cdlua")
+
+plot = iup.plot{
+--    TITLE = "Plot Test",
+    MARGINBOTTOM = 20,
+}
+
+plot:Begin(0)
+plot:Add(0, 0)
+plot:Add(1, 1)
+plot:End()
+
+plot:Begin(0)
+plot:End()
+
+local points_x = {0.1, 0.5, 0.7}
+local points_y = {0.1, 0.5, 0.1}
+plot:AddSamples(1,points_x,points_y,#points_x)
+
+dlg = iup.dialog{
+    TITLE = "Plot Test",
+    SIZE = "400x200",
+    plot,
+}
+
+dlg:showxy(iup.CENTER, iup.CENTER)
+
+iup.MainLoop()
