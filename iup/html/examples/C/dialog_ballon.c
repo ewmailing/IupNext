e9a0d42d977f43d25dc269bca622bca7b99a061b
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2019-10-17 19:56:19 +0000

    shapeimage test with resize
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@5442 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/html/examples/C/dialog_ballon.c
+++ iup/html/examples/C/dialog_ballon.c
@@ -8,14 +8,23 @@ int quit_cb(void)
   return IUP_CLOSE;
 }
 
+int resize_cb(Ihandle* dlg, int w, int h)
+{
+  Ihandle* image = IupGetAttributeHandle(dlg, "SHAPEIMAGE");
+  IupSetStrf(image, "RESIZE", "%dx%d", w, h);
+  IupSetAttribute(dlg, "SHAPEIMAGE", NULL);
+  IupSetAttributeHandle(dlg, "SHAPEIMAGE", image);
+  return IUP_DEFAULT;
+}
+
 int main(int argc, char **argv)
 {
   Ihandle *dialog, *quit_bt, *vbox, *image;
 
   IupOpen(&argc, &argv);
 
-//  image = IupLoadImage("D:\\tecgraf\\iup\\html\\examples\\C\\balloon.png");
-  image = IupLoadImage("/tecgraf/iup/html/examples/C/balloon.png");
+  image = IupLoadImage("D:\\tecgraf\\iup\\html\\examples\\C\\balloon.png");
+//  image = IupLoadImage("/tecgraf/iup/html/examples/C/balloon.png");
 //  image = IupLoadImage("/tecgraf/iup/bin/Debug_64/balloon.png");
 
   /* Creating the button */ 
@@ -24,7 +33,7 @@ int main(int argc, char **argv)
 
   /* the container with a label and the button */
   vbox = IupVbox(
-           IupSetAttributes(IupFlatLabel("Very Long Text Label"), "EXPAND=YES, ALIGNMENT=ACENTER, FONTSIZE=28"), 
+           IupSetAttributes(IupFlatLabel("Very Long Text Label"), "EXPAND=YES, ALIGNMENT=ACENTER, FONTSIZE=24"), 
            quit_bt,
            NULL);
   IupSetAttribute(vbox, "ALIGNMENT", "ACENTER");
@@ -40,6 +49,7 @@ int main(int argc, char **argv)
   /* Creating the dialog */ 
   dialog = IupDialog(vbox);
   IupSetAttributeHandle(dialog, "DEFAULTESC", quit_bt);
+//  IupSetCallback(dialog, "RESIZE_CB", (Icallback)resize_cb);
 
   IupSetAttribute(dialog, "BORDER", "NO");
   IupSetAttribute(dialog, "RESIZE", "NO");
