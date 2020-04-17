ef59a9babf9a0e89394c2dd3ceadba9e168fb21b
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2020-04-17 17:44:04 +0000

    New: Reset Value, Properties, Show Auto Names and Check Handles for global names in IupGlobalsDialog.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@5741 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/src/iup_globalsdlg.c
+++ iup/src/iup_globalsdlg.c
@@ -108,29 +108,20 @@ static iRegisteredGlobal attribs[] = {
   { NULL, 0, 0, 0, 0 }
 };
 
-static int compare_names(const void* i1, const void* i2)
+static int iGlobalsCompareRegisteredNames(const void* i1, const void* i2)
 {
   iRegisteredGlobal ih1 = *((iRegisteredGlobal*)i1);
   iRegisteredGlobal ih2 = *((iRegisteredGlobal*)i2);
   char* name1 = ih1.name;
   char* name2 = ih2.name;
-  return iupStrCompare(name1, name2, 1, 0);
+  return strcmp(name1, name2);
 }
 
-static int iGlobalGetRegisteredAttributes(char **names, int total_count)
+static int iGlobalGetRegisteredAttributesCount(void)
 {
-  int count, i;
-
-  count = 0;
+  int count = 0;
   while (attribs[count].name != NULL)
     count++;
-
-  if (total_count == 0 || total_count == -1)
-    return count;
-
-  for (i = 0; i < total_count; i++)
-    names[i] = iupStrDup(attribs[i].name);
-
   return count;
 }
 
@@ -140,11 +131,18 @@ static int iGlobalIsRegistered(char *name)
 
   for (i = 0; attribs[i].name != NULL; i++)
   {
-    if (iupStrCompare(attribs[i].name, name, 1, 0) == 0)
+    if (iupStrEqual(attribs[i].name, name))
       return 1;
   }
 
   return 0;
+}
+
+static int iGlobalsCompareNames(const void* i1, const void* i2)
+{
+  char* name1 = *((char**)i1);
+  char* name2 = *((char**)i2);
+  return strcmp(name1, name2);
 }
 
 static int iGlobalGetAppAttributes(char **names)
@@ -172,23 +170,25 @@ static int iGlobalGetAppAttributes(char **names)
   return count;
 }
 
-static void iGlobalsUpdate(Ihandle* properties)
+static void iGlobalsUpdate(Ihandle* dlg)
 {
-  int i, j, attr_count, total_count = iGlobalGetAppAttributes(NULL);
-  Ihandle* list1 = (Ihandle*)iupAttribGet(properties, "_IUP_PROPLIST1");
+  int i, j, attr_count, total_count;
+  Ihandle* list1 = (Ihandle*)iupAttribGet(dlg, "_IUP_PROPLIST1");
   Ihandle* txt1 = (Ihandle*)iupAttribGetInherit(list1, "_IUP_ATTVALUE");
   Ihandle* setbut1 = (Ihandle*)iupAttribGetInherit(list1, "_IUP_SETBUTTON");
   Ihandle* colorbut1 = (Ihandle*)iupAttribGetInherit(list1, "_IUP_COLORBUTTON");
   Ihandle* fontbut1 = (Ihandle*)iupAttribGetInherit(list1, "_IUP_FONTBUTTON");
-  Ihandle* list2 = (Ihandle*)iupAttribGetInherit(properties, "_IUP_PROPLIST2");
+  Ihandle* list2 = (Ihandle*)iupAttribGetInherit(dlg, "_IUP_PROPLIST2");
   Ihandle* txt2 = (Ihandle*)iupAttribGetInherit(list2, "_IUP_ATTVALUE");
   Ihandle* setbut2 = (Ihandle*)iupAttribGetInherit(list2, "_IUP_SETBUTTON");
   Ihandle* colorbut2 = (Ihandle*)iupAttribGetInherit(list2, "_IUP_COLORBUTTON");
   Ihandle* fontbut2 = (Ihandle*)iupAttribGetInherit(list2, "_IUP_FONTBUTTON");
-  Ihandle* list3 = (Ihandle*)iupAttribGet(properties, "_IUP_FUNCLIST");
-  Ihandle* txt3 = (Ihandle*)iupAttribGetInherit(list3, "_IUP_CBVALUE");
-  Ihandle* list4 = (Ihandle*)iupAttribGet(properties, "_IUP_NAMESLIST");
+  Ihandle* list3 = (Ihandle*)iupAttribGet(dlg, "_IUP_FUNCLIST");
+  Ihandle* txt3 = (Ihandle*)iupAttribGetInherit(list3, "_IUP_FUNCVALUE");
+  Ihandle* list4 = (Ihandle*)iupAttribGet(dlg, "_IUP_NAMESLIST");
   Ihandle* txt4 = (Ihandle*)iupAttribGetInherit(list4, "_IUP_NAMESVALUE");
+  Ihandle* show_auto_tg = IupGetDialogChild(dlg, "SHOWAUTO");
+  int show_auto = IupGetInt(show_auto_tg, "VALUE");
   char **attr_names;
 
   /* Clear everything */
@@ -207,11 +207,12 @@ static void iGlobalsUpdate(Ihandle* properties)
   IupSetAttribute(list4, "REMOVEITEM", NULL);
   IupSetAttribute(txt4, "VALUE", "");
 
+  total_count = iGlobalGetAppAttributes(NULL);
   if (total_count > 0)
   {
     attr_names = (char **)malloc(total_count * sizeof(char *));
     attr_count = iGlobalGetAppAttributes(attr_names);
-    qsort(attribs, total_count, sizeof(iRegisteredGlobal), compare_names);
+    qsort(attr_names, total_count, sizeof(char*), iGlobalsCompareNames);
     for (i = 0; i < attr_count; i++)
     {
       IupSetAttributeId(list2, "", i + 1, attr_names[i]);
@@ -221,8 +222,8 @@ static void iGlobalsUpdate(Ihandle* properties)
     free(attr_names);
   }
 
-  total_count = iGlobalGetRegisteredAttributes(NULL, 0);
-  qsort(attribs, total_count, sizeof(iRegisteredGlobal), compare_names);
+  total_count = iGlobalGetRegisteredAttributesCount();
+  qsort(attribs, total_count, sizeof(iRegisteredGlobal), iGlobalsCompareRegisteredNames);
   j = 0;
   for (i = 0; i < total_count; i++)
   {
@@ -241,14 +242,20 @@ static void iGlobalsUpdate(Ihandle* properties)
   attr_count = iupGetFunctions(attr_names, total_count);
   for (i = 0; i < attr_count; i++)
     IupSetAttributeId(list3, "", i + 1, attr_names[i]);
-
   free(attr_names);
 
   total_count = IupGetAllNames(NULL, 0);
   attr_names = (char **)malloc(total_count * sizeof(char *));
   attr_count = IupGetAllNames(attr_names, total_count);
+  j = 1;
   for (i = 0; i < attr_count; i++)
-    IupSetAttributeId(list4, "", i + 1, attr_names[i]);
+  {
+    if (show_auto || !iupStrEqualPartial(attr_names[i], "_IUP_NAME"))
+    {
+      IupSetAttributeId(list4, "", j, attr_names[i]);
+      j++;
+    }
+  }
 
   free(attr_names);
 }
@@ -377,10 +384,10 @@ static int iGlobalsSetFont_CB(Ihandle *fontbut)
   return IUP_DEFAULT;
 }
 
-static int iGlobalsSetStr_CB(Ihandle* button)
+static int iGlobalsSetNewAttrib_CB(Ihandle* button)
 {
-  char* name = IupGetAttribute(IupGetDialogChild(button, "NAME22"), "VALUE");
-  char* value = IupGetAttribute(IupGetDialogChild(button, "VALUE22"), "VALUE");
+  char* name = IupGetAttribute(IupGetDialogChild(button, "NEW_ATTRIB_NAME"), "VALUE");
+  char* value = IupGetAttribute(IupGetDialogChild(button, "NEW_ATTRIB_VALUE"), "VALUE");
   if (!value || iupStrEqual(value, "NULL"))
     IupSetGlobal(name, NULL);
   else
@@ -391,41 +398,126 @@ static int iGlobalsSetStr_CB(Ihandle* button)
   return IUP_DEFAULT;
 }
 
-static int iGlobalsFunctionsList_CB(Ihandle *list1, char *name, int item, int state)
+static int iGlobalsFunctionsList_CB(Ihandle *list, char *name, int item, int state)
 {
   (void)item;
   if (state)
   {
     Icallback cb = IupGetFunction(name);
-    Ihandle* txt1 = (Ihandle *)iupAttribGetInherit(list1, "_IUP_CBVALUE");
+    Ihandle* txt = (Ihandle *)iupAttribGetInherit(list, "_IUP_FUNCVALUE");
 
     if (cb)
-      IupSetfAttribute(txt1, "VALUE", "%p", cb);
+      IupSetfAttribute(txt, "VALUE", "%p", cb);
     else
-      IupSetAttribute(txt1, "VALUE", "NULL");
-
-    IupSetAttribute(txt1, "READONLY", "YES");
+      IupSetAttribute(txt, "VALUE", "NULL");
   }
 
   return IUP_DEFAULT;
 }
 
-static int iGlobalsNamesList_CB(Ihandle *list1, char *name, int item, int state)
+static int iGlobalsFunctionReset_CB(Ihandle* bt)
+{
+  Ihandle* list = (Ihandle*)iupAttribGetInherit(bt, "_IUP_FUNCLIST");
+  char* name = IupGetAttribute(list, "VALUESTRING");
+  if (name)
+  {
+    Ihandle* txt = (Ihandle *)iupAttribGetInherit(bt, "_IUP_FUNCVALUE");
+
+    IupSetFunction(name, NULL);
+    IupSetAttribute(txt, "VALUE", "NULL");
+    IupSetInt(list, "REMOVEITEM", IupGetInt(list, "VALUE"));
+  }
+  return IUP_DEFAULT;
+}
+
+static int iGlobalsNamesList_CB(Ihandle *list, char *name, int item, int state)
 {
   (void)item;
   if (state)
   {
-    Ihandle* txt1 = (Ihandle *)iupAttribGetInherit(list1, "_IUP_NAMESVALUE");
-    Ihandle* hnd = IupGetHandle(name);
+    Ihandle* txt = (Ihandle *)iupAttribGetInherit(list, "_IUP_NAMESVALUE");
+    Ihandle* elem = IupGetHandle(name);
 
-    if (hnd)
-      IupSetfAttribute(txt1, "VALUE", "%p (%s)", hnd, hnd->iclass->name);
+    if (elem)
+      IupSetfAttribute(txt, "VALUE", "%p (%s)", elem, elem->iclass->name);
     else
-      IupSetAttribute(txt1, "VALUE", "NULL");
-
-    IupSetAttribute(txt1, "READONLY", "YES");
+      IupSetAttribute(txt, "VALUE", "NULL");
   }
 
+  return IUP_DEFAULT;
+}
+
+static int iGlobalsNameReset_CB(Ihandle* bt)
+{
+  Ihandle* list = (Ihandle*)iupAttribGetInherit(bt, "_IUP_NAMESLIST");
+  char* name = IupGetAttribute(list, "VALUESTRING");
+  if (name)
+  {
+    Ihandle* txt = (Ihandle *)iupAttribGetInherit(bt, "_IUP_NAMESVALUE");
+
+    IupSetHandle(name, NULL);
+    IupSetAttribute(txt, "VALUE", "NULL");
+    IupSetInt(list, "REMOVEITEM", IupGetInt(list, "VALUE"));
+  }
+  return IUP_DEFAULT;
+}
+
+static int iGlobalsNameProperties_CB(Ihandle* bt)
+{
+  Ihandle* list = (Ihandle*)iupAttribGetInherit(bt, "_IUP_NAMESLIST");
+  char* name = IupGetAttribute(list, "VALUESTRING");
+  if (name)
+  {
+    Ihandle* elem = IupGetHandle(name);
+    Ihandle* dlg = IupElementPropertiesDialog(IupGetDialog(list), elem);
+    IupPopup(dlg, IUP_CENTERPARENT, IUP_CENTERPARENT);
+    IupDestroy(dlg);
+  }
+  return IUP_DEFAULT;
+}
+
+static int iGlobalsNamesShowAuto_CB(Ihandle* tg, int state)
+{
+  (void)state;
+  iGlobalsUpdate(IupGetDialog(tg));
+  return IUP_DEFAULT;
+}
+
+static int iGlobalsNameCheckHandles_CB(Ihandle* bt)
+{
+  Ihandle* list = (Ihandle*)iupAttribGetInherit(bt, "_IUP_NAMESLIST");
+  int i, count = IupGetInt(list, "COUNT");
+  int log_size = 0, log_max_size = count * 50;
+  char* log = malloc(log_max_size);
+
+  for (i = 0; i < count; i++)
+  {
+    char* name = IupGetAttributeId(list, "", i+1);
+    Ihandle* elem = IupGetHandle(name);
+    if (!iupObjectCheck(elem))
+    {
+      int name_len = (int)strlen(name);
+      if (log_size + name_len + 1 > log_max_size)
+      {
+        log_max_size += 10 * name_len;
+        log = realloc(log, log_max_size);
+      }
+
+      memcpy(log + log_size, name, name_len);
+      memcpy(log + log_size + name_len, "\n", 1);
+      log_size += name_len + 1;
+    }
+  }
+
+  if (log_size != 0)
+  {
+    log[log_size] = 0;
+    IupGetText("Invalid Handles", log, log_size);
+  }
+  else
+    IupMessage("Invalid Handles", "All handles are valid!");
+
+  free(log);
   return IUP_DEFAULT;
 }
 
@@ -508,69 +600,61 @@ static Ihandle* iGlobalsCreateDialog(void)
   list1 = IupList(NULL);
   IupSetCallback(list1, "ACTION", (Icallback)iGlobalsList_CB);
   IupSetAttribute(list1, "VISIBLELINES", "15");
-  IupSetAttribute(list1, "VISIBLECOLUMNS", "11");
+  IupSetAttribute(list1, "VISIBLECOLUMNS", "16");
   IupSetAttribute(list1, "EXPAND", "VERTICAL");
 
   set1 = IupButton("Set", NULL);
   IupSetCallback(set1, "ACTION", iGlobalsSet_CB);
   IupSetStrAttribute(set1, "PADDING", IupGetGlobal("DEFAULTBUTTONPADDING"));
-  IupSetAttribute(set1, "NAME", "SETBUT1");
 
   colorbut1 = IupButton(NULL, NULL);
   IupSetAttribute(colorbut1, "SIZE", "20x10");
   IupStoreAttribute(colorbut1, "BGCOLOR", "0 0 0");
   IupSetCallback(colorbut1, "ACTION", (Icallback)iGlobalsSetColor_CB);
-  IupSetAttribute(colorbut1, "NAME", "SETCOLORBUT1");
   IupSetAttribute(colorbut1, "VISIBLE", "NO");
 
   fontbut1 = IupButton("F", NULL);
   IupSetAttribute(fontbut1, "SIZE", "20x10");
   IupStoreAttribute(fontbut1, "FONT", "Times, Bold Italic 12");
   IupSetCallback(fontbut1, "ACTION", (Icallback)iGlobalsSetFont_CB);
-  IupSetAttribute(fontbut1, "NAME", "SETFONTBUT1");
   IupSetAttribute(fontbut1, "VISIBLE", "NO");
 
   value1 = IupText(NULL);
   IupSetAttribute(value1, "MULTILINE", "Yes");
   IupSetAttribute(value1, "ALIGNMENT", "ALEFT:ATOP");
   IupSetAttribute(value1, "EXPAND", "YES");
-  IupSetAttribute(value1, "NAME", "VALUE1A");
 
   list2 = IupList(NULL);
   IupSetCallback(list2, "ACTION", (Icallback)iGlobalsList_CB);
   IupSetAttribute(list2, "VISIBLELINES", "15");
-  IupSetAttribute(list2, "VISIBLECOLUMNS", "11");
+  IupSetAttribute(list2, "VISIBLECOLUMNS", "16");
   IupSetAttribute(list2, "EXPAND", "VERTICAL");
 
   set2 = IupButton("Set", NULL);
   IupSetCallback(set2, "ACTION", iGlobalsSet_CB);
   IupSetStrAttribute(set2, "PADDING", IupGetGlobal("DEFAULTBUTTONPADDING"));
-  IupSetAttribute(set2, "NAME", "SETBUT2");
 
   colorbut2 = IupButton(NULL, NULL);
   IupSetAttribute(colorbut2, "SIZE", "20x10");
   IupStoreAttribute(colorbut2, "BGCOLOR", "0 0 0");
   IupSetCallback(colorbut2, "ACTION", (Icallback)iGlobalsSetColor_CB);
-  IupSetAttribute(colorbut2, "NAME", "SETCOLORBUT2");
   IupSetAttribute(colorbut2, "VISIBLE", "NO");
 
   fontbut2 = IupButton("F", NULL);
   IupSetAttribute(fontbut2, "SIZE", "20x10");
   IupStoreAttribute(fontbut2, "FONT", "Times, Bold Italic 12");
   IupSetCallback(fontbut2, "ACTION", (Icallback)iGlobalsSetFont_CB);
-  IupSetAttribute(fontbut2, "NAME", "SETFONTBUT2");
   IupSetAttribute(fontbut2, "VISIBLE", "NO");
 
   value2 = IupText(NULL);
   IupSetAttribute(value2, "MULTILINE", "Yes");
   IupSetAttribute(value2, "ALIGNMENT", "ALEFT:ATOP");
   IupSetAttribute(value2, "EXPAND", "YES");
-  IupSetAttribute(value2, "NAME", "VALUE1A");
 
   list3 = IupList(NULL);
   IupSetCallback(list3, "ACTION", (Icallback)iGlobalsFunctionsList_CB);
   IupSetAttribute(list3, "VISIBLELINES", "15");
-  IupSetAttribute(list3, "VISIBLECOLUMNS", "11");
+  IupSetAttribute(list3, "VISIBLECOLUMNS", "16");
   IupSetAttribute(list3, "SORT", "Yes");
   IupSetAttribute(list3, "EXPAND", "VERTICAL");
 
@@ -581,7 +665,7 @@ static Ihandle* iGlobalsCreateDialog(void)
   list4 = IupList(NULL);
   IupSetCallback(list4, "ACTION", (Icallback)iGlobalsNamesList_CB);
   IupSetAttribute(list4, "VISIBLELINES", "15");
-  IupSetAttribute(list4, "VISIBLECOLUMNS", "11");
+  IupSetAttribute(list4, "VISIBLECOLUMNS", "16");
   IupSetAttribute(list4, "SORT", "Yes");
   IupSetAttribute(list4, "EXPAND", "VERTICAL");
 
@@ -605,17 +689,23 @@ static Ihandle* iGlobalsCreateDialog(void)
 
   box13 = IupVbox(
     IupLabel("Value:"),
-    IupHbox(value3, NULL),
+    value3, 
+    IupSetCallbacks(IupSetAttributes(IupButton("Reset Value", NULL), "PADDING=DEFAULTBUTTONPADDING"), "ACTION", iGlobalsFunctionReset_CB, NULL),
     NULL);
   IupSetAttribute(box13, "MARGIN", "0x0");
   IupSetAttribute(box13, "GAP", "0");
 
   box14 = IupVbox(
     IupLabel("Value:"),
-    IupHbox(value4, NULL),
+    value4, 
+    IupSetCallbacks(IupSetAttributes(IupButton("Reset Value", NULL), "PADDING=DEFAULTBUTTONPADDING"), "ACTION", iGlobalsNameReset_CB, NULL),
+    IupSetCallbacks(IupSetAttributes(IupButton("Properties...", NULL), "PADDING=DEFAULTBUTTONPADDING"), "ACTION", iGlobalsNameProperties_CB, NULL),
+    IupSetAttributes(IupFlatSeparator(), "ORIENTATION=HORIZONTAL"), 
+    IupSetCallbacks(IupSetAttributes(IupToggle("Show Auto Names", NULL), "NAME=SHOWAUTO, TIP=\"Show Automatic Generated Names (_IUP_NAME*)\""), "ACTION", (Icallback)iGlobalsNamesShowAuto_CB, NULL),
+    IupSetCallbacks(IupSetAttributes(IupButton("Check Handles...", NULL), "PADDING=DEFAULTBUTTONPADDING"), "ACTION", iGlobalsNameCheckHandles_CB, NULL),
     NULL);
   IupSetAttribute(box14, "MARGIN", "0x0");
-  IupSetAttribute(box14, "GAP", "0");
+  IupSetAttribute(box14, "GAP", "3");
 
   box1 = IupHbox(IupSetAttributes(IupVbox(IupLabel("Name:"), list1, NULL), "MARGIN=0x0, GAP=0"), box11, NULL);
   iupAttribSet(box1, "_IUP_PROPLIST", (char*)list1);
@@ -633,15 +723,15 @@ static Ihandle* iGlobalsCreateDialog(void)
 
   box2 = IupSetAttributes(IupVbox(
     box2,
-    IupSetAttributes(IupFrame(IupSetAttributes(IupHbox(IupSetAttributes(IupVbox(IupLabel("Name:"), IupSetAttributes(IupText(NULL), "VISIBLECOLUMNS=9, NAME=NAME22"), NULL), "GAP=0, MARGIN=5x5"),
-    IupSetAttributes(IupVbox(IupLabel("Value:"), IupSetAttributes(IupText(NULL), "EXPAND=HORIZONTAL, NAME=VALUE22"), NULL), "GAP=0, MARGIN=5x5"),
-    IupSetAttributes(IupVbox(IupLabel(""), IupSetCallbacks(IupSetAttributes(IupButton("Set", NULL), "PADDING=3x0"), "ACTION", iGlobalsSetStr_CB, NULL), NULL), "GAP=0, MARGIN=5x5"),
-    NULL), "ALIGNMENT=ACENTER")), "TITLE=\"New Attribute\""),
+    IupSetAttributes(IupFrame(IupSetAttributes(IupHbox(IupSetAttributes(IupVbox(IupLabel("Name:"), IupSetAttributes(IupText(NULL), "VISIBLECOLUMNS=9, NAME=NEW_ATTRIB_NAME"), NULL), "GAP=0, MARGIN=5x5"),
+    IupSetAttributes(IupVbox(IupLabel("Value:"), IupSetAttributes(IupText(NULL), "EXPAND=HORIZONTAL, NAME=NEW_ATTRIB_VALUE"), NULL), "GAP=0, MARGIN=5x5"),
+    IupSetAttributes(IupVbox(IupLabel(""), IupSetCallbacks(IupSetAttributes(IupButton("Set", NULL), "PADDING=3x0, TIP=\"Sets an attribute value. Actually can be any attribute, registered or custom.\""), "ACTION", iGlobalsSetNewAttrib_CB, NULL), NULL), "GAP=0, MARGIN=5x5"),
+    NULL), "ALIGNMENT=ACENTER")), "TITLE=\"Set Attribute\""),
     NULL), "MARGIN=0x0");
 
   box3 = IupHbox(IupSetAttributes(IupVbox(IupLabel("Name:"), list3, NULL), "MARGIN=0x0, GAP=0"), box13, NULL);
   iupAttribSet(box3, "_IUP_FUNCLIST", (char*)list3);
-  iupAttribSet(box3, "_IUP_CBVALUE", (char*)value3);
+  iupAttribSet(box3, "_IUP_FUNCVALUE", (char*)value3);
 
   box4 = IupHbox(IupSetAttributes(IupVbox(IupLabel("Name:"), list4, NULL), "MARGIN=0x0, GAP=0"), box14, NULL);
   iupAttribSet(box4, "_IUP_NAMESLIST", (char*)list4);
