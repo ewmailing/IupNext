7f2e3af4d400374bfd7c467b6f4f19a6df7ec239
scuri <scuri@2f08bc20-984a-4df8-b195-9b5d5d477537>
2020-07-31 18:28:43 +0000

    Changed: pre-compiled binaries of the tools executables are now built with Visual C++ 15 (Visual Studio 2019) and they are NOT compatible with Windows XP nor Windows Vista, only with Windows 7, 8 and 10. Although they can still be built from source to run on Windows XP/Vista.
    git-svn-id: svn://svn.code.sf.net/p/iup/iup/trunk@5890 2f08bc20-984a-4df8-b195-9b5d5d477537

--- iup/srcluaconsole/make_uname.bat
+++ iup/srcluaconsole/make_uname.bat
@@ -1,8 +1,8 @@
 @echo off  
 
 if "%1"==""        goto iupexe32
-if "%1"=="vc10"    goto iupexe32
-if "%1"=="vc10_64" goto iupexe64
+if "%1"=="vc15"    goto iupexe32
+if "%1"=="vc15_64" goto iupexe64
 if "%1"=="cygw17"  goto iupexecygw
 if "%1"=="all"     goto iupexeall
 goto end
@@ -10,11 +10,11 @@ goto end
 REM Must use dll* so USE_* will use the correct TEC_UNAME
 
 :iupexe32
-call tecmake dll10 %2 %3 %4 %5 %6 %7
+call tecmake dll15 %2 %3 %4 %5 %6 %7
 goto end
 
 :iupexe64
-call tecmake dll10_64 %2 %3 %4 %5 %6 %7
+call tecmake dll15_64 %2 %3 %4 %5 %6 %7
 goto end
 
 :iupexecygw
@@ -22,8 +22,8 @@ call tecmake cygw17 %2 %3 %4 %5 %6 %7
 goto end
 
 :iupexeall
-call make_uname vc10 %2 %3 %4 %5 %6 %7
-call make_uname vc10_64 %2 %3 %4 %5 %6 %7
+call make_uname vc15 %2 %3 %4 %5 %6 %7
+call make_uname vc15_64 %2 %3 %4 %5 %6 %7
 REM call make_uname cygw17 %2 %3 %4 %5 %6 %7
 goto end
 
